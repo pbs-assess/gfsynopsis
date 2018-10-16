e82e65531a0720e09c34a76897dc3e7bc9769cdd
Sean Anderson <sean@seananderson.ca>
2018-10-16 16:44:59 -0700

    Quickly filter all CSAS publications to look for missing references #45

---
+++ report/refs.R
@@ -0,0 +1,40 @@
+library(dplyr)
+check_refs <- function(file) {
+  sr <- readxl::read_excel(file)
+  sr$`TITLE FRENCH` <- NULL
+  names(sr) <- tolower(names(sr))
+  sr$id <- seq_len(nrow(sr))
+
+  spp <- gfsynopsis::get_spp_names()
+
+  check <- function(dat, spp_name1, spp_name2) {
+    i1 <- grep(spp_name1, dat$`title english`, ignore.case = TRUE)
+    i2 <- grep(spp_name2, dat$`title english`, ignore.case = TRUE)
+    union(i1, i2)
+  }
+
+  out <- purrr::map2_df(
+    .x = spp$species_common_name,
+    .y = spp$species_science_name, function(.x, .y) {
+    ii <- check(sr, .x, .y)
+    if (length(ii) > 0L)
+      data.frame(species_common_name = .x, id = ii,
+        stringsAsFactors = FALSE)
+    }
+  )
+  out <- dplyr::left_join(out, sr) %>%
+    arrange(series, year, species_common_name)
+
+  out
+}
+
+out <- list()
+out[[1]] <- check_refs("inst/extdata/sr-2018-10-16.xlsx")
+out[[2]] <- check_refs("inst/extdata/resdocs-2018-10-16.xlsx")
+out[[3]] <- check_refs("inst/extdata/sar-2018-10-16.xlsx")
+out[[4]] <- check_refs("inst/extdata/all-advisory-2018-10-16.xlsx")
+out <- bind_rows(out)
+out <- out[!duplicated(out),, drop=FALSE]
+out <- arrange(out, species_common_name, year, series)
+# View(out)
+readr::write_csv(out, "report/csas-filtered.csv")
