e86463cb677d2e7b684b056a6b75daab2a660bc5
Sean Anderson <sean@seananderson.ca>
2018-10-24 15:42:10 -0700

    Mean is not part of stats

--- R/utils.R
+++ R/utils.R
@@ -48,7 +48,7 @@ fourth_root_power_trans <- function() {
 }
 
 find_length_outliers <- function(xx) {
-  yy <-  stats::pnorm(xx, mean = stats:mean(xx, na.rm = TRUE),
+  yy <-  stats::pnorm(xx, mean = mean(xx, na.rm = TRUE),
     sd = stats::sd(xx, na.rm = TRUE), log.p = TRUE)
   zz <- stats::qnorm(yy, log.p = TRUE)
   out <- zz[zz > 4 & !is.na(zz)]
