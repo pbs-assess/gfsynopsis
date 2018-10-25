1a7c43bb42d292766e00a495c90419b758f78091
andrew-edwards <Andrew.Edwards@dfo-mpo.gc.ca>
2018-10-24 17:00:15 -0700

    Extract the IPHC data for hook with bait (needs GFbio).

--- R/data.R
+++ R/data.R
@@ -24,6 +24,8 @@ get_data <- function(type = c("A", "B"), path = ".",
   }
   get_data_iphc(type = type, path = paste0(path, "/iphc"),
                 compress = compress, force = force)
+  get_data_iphc_hook_with_bait(path = paste0(path, "/iphc"),
+                compress = compress, force = force)
 }
 
 #' Get the IPHC data for all years, should get merged into get_data at some point
@@ -46,5 +48,26 @@ get_data_iphc <- function(type = c("A", "B"), path = ".",
     gfplot::cache_pbs_data_iphc(species = .d$species_common_name,
       file_name = .d$spp_w_hyphens,
       path = path, compress = compress)
+}
+
+#' Get the IPHC data for all years for hooks with bait (for hook competition
+#'  calculations.
+#' @param path Path
+#' @param compress TRUE or FALSE to compress .rds
+#' @param force Should data be downloaded even if already cached data exists?
+#' @export
+#'
+get_data_iphc_hook_with_bait <- function(path = ".",
+  compress = FALSE, force = FALSE) {
+  dir.create(path, showWarnings = FALSE)
+  .d <- "hook-with-bait"
+#  .d <- .d[.d$type %in% type, , drop = FALSE]
+  already_exists <- gsub("\\.rds", "", list.files(path))
+  if (!force & file.exists(paste0(path, "hook-with-bait.rds"))) {
+     return() } else {
+  gfplot::cache_pbs_data_iphc(species = "hook with bait",
+    file_name = "hook-with-bait",
+    path = path, compress = compress)
+  }
 }
 
