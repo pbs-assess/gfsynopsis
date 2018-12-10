09e8ddeaee36384828b07f26c30cd70077284a78
Sean Anderson <sean@seananderson.ca>
2018-12-10 12:12:11 -0800

    Catch -> Canadian catch; closes #112

--- R/catch.R
+++ R/catch.R
@@ -48,7 +48,8 @@ plot_catches <- function(dat, blank_plot = FALSE, xlim = c(1955, 2017), ...) {
     legend.position = c(0, 0.215),
     legend.background = element_blank(),
     legend.direction = "horizontal") +
-    ggplot2::guides(fill = ggplot2::guide_legend(nrow = 2))
+    ggplot2::guides(fill = ggplot2::guide_legend(nrow = 2)) +
+    ggplot2::title("Canadian catch")
 
   g
 }
