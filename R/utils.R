14cdc0886fabf219cbb8a5086830e3559cd9355d
Sean Anderson <sean@seananderson.ca>
2019-08-23 12:10:12 -0700

    Add cap()

--- R/utils.R
+++ R/utils.R
@@ -21,10 +21,11 @@ all_cap <- function(x) toupper(x)
 
 emph <- function(x) paste0("\\emph{", x, "}")
 
+cap <- function(s, strict = FALSE) paste(toupper(substring(s, 1, 1)),
+  {s <- substring(s, 2); if(strict) tolower(s) else s},
+  sep = "", collapse = " " )
+
 first_cap <- function(s, strict = FALSE) {
-  cap <- function(s) paste(toupper(substring(s, 1, 1)),
-    {s <- substring(s, 2); if(strict) tolower(s) else s},
-    sep = "", collapse = " " )
   sapply(strsplit(s, split = " "), cap, USE.NAMES = !is.null(names(s)))
 }
 
