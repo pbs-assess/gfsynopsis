af8c75c1905a72d494171700e971be223ce67179
Sean Anderson <sean@seananderson.ca>
2019-12-03 17:17:18 -0800

    Update compile.R

--- paper/compile.R
+++ paper/compile.R
@@ -1,15 +1,15 @@
 rmarkdown::render("paper.Rmd")
-d <- readLines("paper.tex")
+# d <- readLines("paper.tex")
 
-figure_line <- grep("^\\\\begin\\{figure\\}", d)[[1]]
-bib_beg_line <- grep("^\\\\hypertarget\\{references", d)[[1]]
-bib_end_line <- length(d) - 2
+# figure_line <- grep("^\\\\begin\\{figure\\}", d)[[1]]
+# bib_beg_line <- grep("^\\\\hypertarget\\{references", d)[[1]]
+# bib_end_line <- length(d) - 2
 
-d <- d[c(
-  1:(figure_line - 1),
-  bib_beg_line:bib_end_line,
-  c(figure_line:bib_beg_line - 1), length(d)
-)]
-writeLines(d, "paper.tex")
+# d <- d[c(
+#   1:(figure_line - 1),
+#   bib_beg_line:bib_end_line,
+#   c(figure_line:bib_beg_line - 1), length(d)
+# )]
+# writeLines(d, "paper.tex")
 
-tinytex::latexmk("paper.tex")
+# tinytex::latexmk("paper.tex")
