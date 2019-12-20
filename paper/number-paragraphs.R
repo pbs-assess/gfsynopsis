73534be02a3288df9334e731f21baa03726d8dbe
Sean Anderson <sean@seananderson.ca>
2019-12-20 12:06:36 -0500

    Rearrange methods paragraphs

--- paper/number-paragraphs.R
+++ paper/number-paragraphs.R
@@ -1,10 +1,13 @@
-d <- readLines("paper.Rmd")
-j <- 1
-for (i in seq_along(d)) {
-  p <- grepl("<!-- Paragraph [0-9]+", d[i])
-  if (p) {
-    d[i] <- gsub("(Paragraph) ([0-9]+):", paste0("\\1 ", j, ":"), d[i])
-    j <- j + 1
+number_paragraphs <- function() {
+  d <- readLines("paper.Rmd")
+  j <- 1
+  for (i in seq_along(d)) {
+    p <- grepl("<!-- Paragraph [0-9]+", d[i])
+    if (p) {
+      d[i] <- gsub("(Paragraph) ([0-9]+):", paste0("\\1 ", j, ":"), d[i])
+      j <- j + 1
+    }
   }
+  writeLines(d, "paper.Rmd")
 }
-writeLines(d, "paper.Rmd")
+number_paragraphs()
