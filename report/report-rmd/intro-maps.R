7ff14e09b1914b496c25919f7754245944ee6846
ecophilina <paenglis@lakeheadu.ca>
2022-03-30 10:55:33 -0700

    Add inside HBLL to fig 1 map

--- report/report-rmd/intro-maps.R
+++ report/report-rmd/intro-maps.R
@@ -30,7 +30,9 @@ g <- ggplot()
 
 cols <- paste0(c(RColorBrewer::brewer.pal(5L, "Set1"),
   RColorBrewer::brewer.pal(8L, "Set1")[7:8],
-  "#303030", "#a8a8a8", "#a8a8a8", "#a8a8a8"), "80")
+  # "#303030", "#a8a8a8", "#a8a8a8", "#a8a8a8"), "80")
+  "#303030", "#60b6bb", "#1d989e", "#a8a8a8"), "80")
+
 g <- g + geom_polygon(data = ss, aes_string(x = "X", y = "Y", fill = "survey")) +
   scale_fill_manual(values = c(
     "Hecate Strait (HS)" = cols[2],
@@ -61,16 +63,25 @@ g <- g + theme(legend.justification = c(0, 0), legend.position = c(0, 0))
 hbll_n <- gfplot:::ll2utm(gfplot::hbll_n_grid$grid, utm_zone = 9)
 hbll_s <- gfplot:::ll2utm(gfplot::hbll_s_grid$grid, utm_zone = 9)
 
+hbll_n_in <- gfplot:::ll2utm(gfplot::hbll_inside_n_grid$grid, utm_zone = 9)
+hbll_s_in <- gfplot:::ll2utm(gfplot::hbll_inside_s_grid$grid, utm_zone = 9)
+
 hbll <- dplyr::bind_rows(
-  list(data.frame(hbll_n, survey = "Outside Hard Bottom Long Line (N)", stringsAsFactors = FALSE),
-    data.frame(hbll_s, survey = "Outside Hard Bottom Long Line (S)", stringsAsFactors = FALSE)))
+  list(
+    data.frame(hbll_n, survey = "Outside Hard Bottom Long Line (N)", stringsAsFactors = FALSE),
+    data.frame(hbll_s, survey = "Outside Hard Bottom Long Line (S)", stringsAsFactors = FALSE),
+    data.frame(hbll_n_in, survey = "Inside Hard Bottom Long Line (N)", stringsAsFactors = FALSE),
+    data.frame(hbll_s_in, survey = "Inside Hard Bottom Long Line (S)", stringsAsFactors = FALSE)))
 
 g2 <- ggplot()
 g2 <- g2 + geom_rect(data = hbll,
   aes_string(xmax = "X + 1", ymax = "Y + 1", xmin = "X - 1", ymin = "Y - 1", fill = "survey")) +
   scale_fill_manual(values = c(
     "Outside Hard Bottom Long Line (N)" = cols[5],
-    "Outside Hard Bottom Long Line (S)" = cols[6])) +
+    "Outside Hard Bottom Long Line (S)" = cols[6],
+    "Inside Hard Bottom Long Line (N)" = cols[9],
+    "Inside Hard Bottom Long Line (S)" = cols[10]
+    )) +
   geom_path(
     data = isobath_utm, aes_string(
       x = "X", y = "Y",
