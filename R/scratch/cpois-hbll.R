2277c0010d5f8589b07fb127834f18d4bb798820
Jillian Dunic <>
2023-08-18 10:59:31 -0700

    simplify data structures since cpois is poisson when upr = catch

--- R/scratch/cpois-hbll.R
+++ R/scratch/cpois-hbll.R
@@ -85,14 +85,13 @@ pstar_df <-
 # Stitch HBLL index using censored poisson where applicable
 model_type <- "st-rw"
 # family <- sdmTMB::censored_poisson()
-# family <- sdmTMB::nbinom1(link = "log")
+family <- sdmTMB::nbinom1(link = "log")
 # family <- poisson(link = "log")
 pstar_df <- readRDS(file.path(pstar_cache, survey_type, 'pstar-df.rds')) |>
-  rename(pstar = "prop_removed") |> # This still needs to be updated for hbll out and ins
   right_join(tibble(species_common_name = spp_list))
 
 add_upr <- function(dat, prop_removed_col, n_catch_col, n_hooks_col,
-  pstar_col = 'pstar', cap_upr = FALSE, pstar = NULL) {
+  pstar_col = 'pstar', pstar = NULL) {
   if (is.null(pstar)) {
     pstar <- dat[[pstar_col]][1]
   }
@@ -104,10 +103,6 @@ add_upr <- function(dat, prop_removed_col, n_catch_col, n_hooks_col,
         dat[[n_hooks_col]], pstar)
   }
 
-  if (cap_upr) {
-    dat <- dat |> mutate(upr = ifelse((upr < hook_count) | is.na(upr),  upr, dat[[n_hooks_col]]))
-  }
-
   if (all(is.na(dat$upr))) {
     control_upr <- NULL
   }  else {
@@ -117,10 +112,21 @@ add_upr <- function(dat, prop_removed_col, n_catch_col, n_hooks_col,
   list(survey_dat = dat, control_upr = control_upr)
 }
 
-# @QUESTION should the default be negbin1 or poisson?
+add_upr <- function(dat, prop_removed_col, n_catch_col, n_hooks_col,
+  pstar_col = 'pstar', pstar = NULL) {
+  if (is.null(pstar)) {
+    pstar <- dat[[pstar_col]][1]
+  }
+
+  dat$upr <- sdmTMB:::get_censored_upper(dat[[prop_removed_col]], dat[[n_catch_col]],
+        dat[[n_hooks_col]], pstar)
+  return(dat)
+}
+
+# Use poisson as default to be consistent with using censored poisson
 get_pois_flavour <- function(upr) {
   if (is.null(upr)) {
-    family <- sdmTMB::nbinom1(link = "log")
+    family <- poisson(link = "log")
   } else {
     family <- sdmTMB::censored_poisson(link = "log")
   }
@@ -129,43 +135,45 @@ get_pois_flavour <- function(upr) {
 
 hbll_stitch_dat <- spp_dat |>
   map(\(dat) left_join(dat, pstar_df, by = 'species_common_name')) |>
-  map(\(dat) add_upr(dat, 'prop_removed', 'catch', 'hook_count', 'pstar',
-    cap_upr = FALSE)) |>
-  map(\(dat) list(survey_dat = dat$survey_dat, control_upr = dat$control_upr,
-    family = get_pois_flavour(dat$control_upr))) |>
+  map(\(dat) mutate(dat, pstar = ifelse(is.na(pstar), 1, pstar))) |>
+  map(\(dat) add_upr(dat, 'prop_removed', 'catch', 'hook_count', 'pstar')) |>
   setNames(spp_list) # makes easier to interactively check species/outputs
 
 # GET INDEX
 # ------------------------------------------------------------------------------
 nbin_index <-
   names(hbll_stitch_dat) |>
-  map(\(sp) get_stitched_index(survey_dat = hbll_stitch_dat[[sp]]$survey_dat, species = sp,
+  map(\(sp) get_stitched_index(survey_dat = hbll_stitch_dat[[sp]], species = sp,
     survey_type = survey_type, model_type = model_type,
     offset = 'offset', # original offset with hook competition
-    family = sdmTMB::nbinom1(link = "log"), ctrl = sdmTMB::sdmTMBcontrol(
-      censored_upper = NULL),
+    family = sdmTMB::nbinom1(link = "log"),
     grid_dir = grid_dir, silent = FALSE,
     cache = file.path(stitch_cache_hbll))
   )
 
-# @QUESTION: should the offset be plain log(hook_count)?
 index_list <-
   names(hbll_stitch_dat) |>
-  map(\(sp) get_stitched_index(survey_dat = hbll_stitch_dat[[sp]]$survey_dat, species = sp,
+  map(\(sp) get_stitched_index(survey_dat = hbll_stitch_dat[[sp]], species = sp,
     survey_type = survey_type, model_type = model_type,
     offset = 'log_hook_count',
-    family = hbll_stitch_dat[[sp]]$family, ctrl = sdmTMB::sdmTMBcontrol(
-      censored_upper = hbll_stitch_dat[[sp]]$control_upr),
+    family = sdmTMB::censored_poisson(link = "log"), ctrl = sdmTMB::sdmTMBcontrol(
+      censored_upper = hbll_stitch_dat[[sp]]$upr),
     grid_dir = grid_dir, silent = FALSE,
     cache = file.path(stitch_cache_hbll, 'cpois'))
   )
-#beepr::beep()
+beepr::beep()
 
 # ------------------------------------------------------------------------------
 
+# stats_family_lu <- hbll_stitch_dat |>
+#   imap(\(x, idx) tibble(species = idx, family = x$family$family[1])) |>
+#   bind_rows()
+
 stats_family_lu <- hbll_stitch_dat |>
-  imap(\(x, idx) tibble(species = idx, family = x$family$family[1])) |>
-  bind_rows()
+  map(\(dat) mutate(dat, family = ifelse(pstar == 1, 'poisson', 'censored poisson'))) |>
+  bind_rows() |>
+  distinct(species_common_name, pstar, family)
+
 
 # @FIXME need to deal with file paths at some point
 index_list <-
@@ -174,7 +182,7 @@ index_list <-
     file.path(stitch_cache_hbll, 'cpois', paste0(gfsynopsis:::clean_name(sp), '_', model_type, '.rds')))) |>
   setNames(spp_list) |>
   keep(is.data.frame) |>
-  imap(\(x, idx) mutate(x, survey_abbrev = "HBLL OUT N/S", species = idx) |>
+  imap(\(x, idx) mutate(x, survey_abbrev = "HBLL OUT N/S", species_common_name = idx) |>
     left_join(stats_family_lu)
   )
 
@@ -200,7 +208,7 @@ cpois_plots <-
           xlim = c(1984 - 0.2, 2022 + 0.2), french = FALSE) +
           scale_x_continuous(guide = ggplot2::guide_axis(check.overlap = TRUE))
   ) |>
-  imap(\(x, idx) x + ggtitle(paste0(index_list[[idx]]$species, ': ', index_list[[idx]]$family)))
+  imap(\(x, idx) x + ggtitle(paste0(index_list[[idx]]$species_common_name, ': ', index_list[[idx]]$family)))
 
 save_plots_to_pdf(cpois_plots, 'scratch-out/hbll_out_cpois_plots.pdf', width = 7, height = 3.7)
 
