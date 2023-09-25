7675c8111c20b405bf3e3d57f8e24b070e8f7db2
Jillian Dunic <>
2023-09-25 11:40:14 -0700

    deal with eulachon and multiple COSEWIC statuses across populations

--- report/make.R
+++ report/make.R
@@ -97,14 +97,27 @@ spp$family[spp$species_common_name == "deacon rockfish"] <-
 cos <- readr::read_csv(here::here("report/COSEWIC-species.csv"), show_col_types = FALSE)
 cos <- dplyr::filter(cos, !grepl("Salmon", `COSEWIC common name`))
 cos <- dplyr::filter(cos, !grepl("Trout", `COSEWIC common name`))
-cos <- dplyr::filter(cos, !grepl("Eulachon", `COSEWIC common name`))
 cos <- dplyr::filter(cos, !grepl("Pixie Poacher", `COSEWIC common name`))
-cos <- rename(cos, species_science_name = `Scientific name`, cosewic_status = `COSEWIC status`, sara_status = `Schedule status`)
+cos <- rename(cos, species_science_name = `Scientific name`,
+  cosewic_status = `COSEWIC status`, sara_status = `Schedule status`,
+  cosewic_pop = `COSEWIC population`)
+# For species with different cosewic statuses for different populations, combine the text:
+cos_multi_pop <- cos |>
+  group_by(species_science_name) |>
+  filter(n_distinct(cosewic_status) > 1) |>
+  group_by(cosewic_status, .add = TRUE) |>
+  summarise(cosewic_pop_status =
+      paste0(unique(cosewic_status), " (", paste(unique(cosewic_pop), collapse = ", "), ")"),
+    .groups = 'drop_last') |>
+  summarise(cosewic_pop_status = paste0(unique(cosewic_pop_status), collapse = ", "), .groups = 'drop')
 # duplicate of inside YE:
 cos <- select(cos, species_science_name, cosewic_status, sara_status)
-cos <- mutate(cos, species_science_name = ifelse(grepl("type I", species_science_name), "Sebastes aleutianus/melanostictus complex", species_science_name)) |>
-  distinct() # to remove duplicates (e.g., YE and RE/BS rockfish)
+cos <- mutate(cos, species_science_name = ifelse(grepl("type I", species_science_name), "Sebastes aleutianus/melanostictus complex", species_science_name))
+cos <- left_join(cos, cos_multi_pop, by = "species_science_name") %>%
+  mutate(cosewic_status = ifelse(!is.na(cosewic_pop_status), cosewic_pop_status, cosewic_status)) %>%
+  select(-cosewic_pop_status)
 cos$species_science_name <- tolower(cos$species_science_name)
+cos <- distinct(cos) # to remove duplicates (e.g., Eulachon, YE, RE/BS rockfish)
 spp <- left_join(spp, cos, by = "species_science_name")
 
 # ------------------------------------------------------------------------------
