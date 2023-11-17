31cfe7866a43ae225209c12f03cd86ff0fc38121
Jillian Dunic <>
2023-11-16 16:26:56 -0800

    fix failed merging and update plots

--- report/mssm-appendix/mssm.R
+++ report/mssm-appendix/mssm.R
@@ -299,6 +299,7 @@ grid_plot + geom_sf(data = gfbio_grid, alpha = 0, colour = NA) +
 
 ggsave(file.path(mssm_figs, 'grid-prediction-2009_no-points.png'), width = 3.5, height = 3.7)
 
+
 grid_plot_2009_points <- mssm_grid_sf |>
   filter(last_samp_year >= 2009 & last_samp_year <= 2019) |>
   ggplot() +
@@ -400,24 +401,27 @@ yearbin_catch <- mssm_dat |>
 post_2003_spp <- filter(yearbin_catch, yearbin_catch == 0)$species_common_name
 saveRDS(post_2003_spp, file.path(mssm_appendix, 'post-2003-spp.rds'))
 
-sampling_2003_plot <-
+sampling_2003 <-
   mssm_dat |>
     group_by(species_common_name, species_code, year) |>
     summarise(mean_catch = mean(catch, na.rm = TRUE), .groups = 'drop') |>
     left_join(yearbin_catch) |>
-    mutate(mean_catch = ifelse((species_common_name %in% post_2003_spp & year < 2003), NA, mean_catch)) |>
-    mutate(species_common_name = stringr::str_to_title(species_common_name)) |>
-    mutate(species_common_name = forcats::fct_reorder(species_common_name, species_code)) |>
-  ggplot(data = _, aes(x = year, y = mean_catch)) +
+    mutate(mean_catch = ifelse((species_common_name %in% post_2003_spp & year < 2003), NA, mean_catch))
+
+pre_2003_spp_plot <- sampling_2003 |>
+  filter(!(species_common_name %in% post_2003_spp)) |>
+  mutate(species_common_name = stringr::str_to_title(species_common_name)) |>
+  mutate(species_common_name = forcats::fct_reorder(species_common_name, species_code)) |>
+ggplot(data = _, aes(x = year, y = mean_catch)) +
     geom_rect(aes(xmin = -Inf, xmax = 2003, ymin = -Inf, ymax = Inf),
               fill = "gray85", alpha = 0.2) +
     geom_point() +
     geom_line(alpha = 0.5) +
     geom_vline(xintercept = 2001, colour = 'grey50') +
     theme(axis.text = element_blank()) +
-    facet_wrap(~ species_common_name, scales = 'free_y') +
-    ggtitle("Mean catch, highlighting species that show up only after 2003")
-sampling_2003_plot
+    facet_wrap(~ species_common_name, scales = 'free_y', ncol = 4) +
+    labs(x =  "Year", y = "Mean annual catch (kg)")
+pre_2003_spp_plot
 
 ggsave(file.path(mssm_figs, 'sampling-2003.png'), plot = sampling_2003_plot,
   width = 17, height = 9)
@@ -451,8 +455,8 @@ ggsave(file.path(mssm_figs, 'sampling-cod.png'), plot = cod_comparison,
 ## Get model where pre and post 2003 is added as factor
 # Use 3km grid
 # All species ID'd to lowest taxonomic level in 2003 onward
-
-furrr::future_walk(spp_vector, function(.sp) {
+future::plan(future::multicore, workers = 8)
+#furrr::future_walk(spp_vector, function(.sp) {
 #purrr::walk(spp_vector, function(.sp) {
   spp_filename <- paste0(gfsynopsis:::clean_name(.sp), "_st-rw.rds")
     survey_dat <- readRDS(file.path(data_cache, paste0(gfsynopsis:::clean_name(.sp), ".rds")))$survey_sets |>
@@ -480,6 +484,7 @@ furrr::future_walk(spp_vector, function(.sp) {
     }
 })
 
+future::plan(future::sequential)
 beepr::beep()
 
 # Use 2km grid
@@ -510,11 +515,12 @@ furrr::future_walk(spp_vector, function(.sp) {
     }
 })
 
+future::plan(future::sequential)
 beepr::beep()
 
 
 # # Fit SYN WCVI ------------------
-furrr::future_walk(spp_vector, function(.sp) {
+purrr::walk(spp_vector, function(.sp) {
   spp_filename <- paste0(gfsynopsis:::clean_name(.sp), "_st-rw.rds")
     survey_dat <- readRDS(file.path(data_cache, paste0(gfsynopsis:::clean_name(.sp), ".rds")))$survey_sets |>
       filter(survey_abbrev == "SYN WCVI")
@@ -776,7 +782,7 @@ mssm_design_only_inds <-
          survey_abbrev %in% c("MSSM Design")) |>
 ggplot(data = _, aes(x = year, y = mssm_scaled_biomass, colour = survey_abbrev, fill = survey_abbrev)) +
   geom_pointrange(aes(ymin = mssm_scaled_lowerci, ymax = mssm_scaled_upperci), size = 0.2) +
-  geom_ribbon(aes(ymin = mssm_scaled_lowerci, ymax = mssm_scaled_upperci), linewidth = 0.2, alpha = 0.1, colour = NA) +
+  geom_ribbon(aes(ymin = mssm_scaled_lowerci, ymax = mssm_scaled_upperci), size = 0.2, alpha = 0.1, colour = NA) +
   scale_colour_manual(values = survey_cols) +
   scale_fill_manual(values = survey_cols) +
   facet_wrap(~ species, scale = 'free_y', ncol = 4) +
@@ -805,7 +811,7 @@ ggplot(data = _, aes(x = year, y = mssm_scaled_biomass, colour = survey_abbrev, 
 mssm_model_design_inds
 
 ggsave(file.path(mssm_figs, 'index-model-design.png'), plot = mssm_model_design_inds,
-  width = 10.5, height = 7)
+  width = 10.5, height = 10)
 
 mssm_syn_inds <- scaled_inds |>
   filter(species %in% spp_in_mssm,
@@ -820,7 +826,7 @@ geom_rect(data = . %>% filter(!(species %in% unique(syn_inds$species))) %>%
 mssm_syn_inds
 
 ggsave(file.path(mssm_figs, 'index-mssm-model-syn-wcvi-model.png'), plot = mssm_syn_inds,
-  width = 10.5, height = 8)
+  width = 10.5, height = 10)
 
 mssm_syn_inds_mssm_grid <- scaled_inds |>
   filter(species %in% spp_in_mssm,
@@ -836,7 +842,7 @@ ggplot(data = _, aes(x = year, y = syn_scaled_biomass, colour = survey_abbrev, f
 mssm_syn_inds_mssm_grid
 
 ggsave(file.path(mssm_figs, 'index-mssm-model-syn-wcvi-model-mssm-grid.png'), plot = mssm_syn_inds_mssm_grid,
-  width = 10.5, height = 8)
+  width = 10.5, height = 10)
 
 
 mssm_syn_grid_zoom_in <- scaled_inds |>
@@ -850,7 +856,7 @@ ggplot(data = _, aes(x = year, y = syn_scaled_biomass, colour = survey_abbrev, f
     "MSSM Model" = "#1b9e77"),
     ncol = 4)
 mssm_syn_grid_zoom_in
-ggsave(file.path(mssm_figs, 'index-mssm-syn-wcvi-mssm-grid-zoom-in.png'), plot = mssm_syn_inds_mssm_grid,
+ggsave(file.path(mssm_figs, 'index-mssm-syn-wcvi-mssm-grid-zoom-in.png'), plot = mssm_syn_grid_zoom_in,
   width = 9.5, height = 11)
 
 mssm_cpue_inds <- scaled_inds |>
@@ -1021,7 +1027,8 @@ size_dat <- spp_vector |>
 
 
 size_summary <- size_dat |>
-  filter(species_common_name %in% spp_in_mssm) |>
+  #filter(species_common_name %in% spp_in_mssm) |>
+  filter(survey_abbrev %in% c('MSSM WCVI', 'SYN WCVI')) |>
   group_by(species_common_name, survey_abbrev) |>
   summarise(q50 = quantile(length, 0.5, na.rm = TRUE),
             q25 = quantile(length, 0.25, na.rm = TRUE),
@@ -1063,18 +1070,20 @@ size_comp <-
 size_comp
 
 ggsave(file.path(mssm_figs, 'size-comp.png'), plot = size_comp,
-  width = 8, height = 5)
+  width = 13, height = 4.5)
 
 age_summary <- size_dat |>
-  filter(species_common_name %in% spp_in_mssm) |>
+  #filter(species_common_name %in% spp_in_mssm) |>
+  filter(survey_abbrev %in% c('MSSM WCVI', 'SYN WCVI')) |>
   group_by(species_common_name, survey_abbrev) |>
+  filter(!is.na(age)) |>
   summarise(q50 = quantile(age, 0.5, na.rm = TRUE),
             q25 = quantile(age, 0.25, na.rm = TRUE),
             q75 = quantile(age, 0.75, na.rm = TRUE),
-            n = sum(!is.na(age))) |>
-  mutate(mean_age = mean(q50)) |>
+            n = n()) |>
+  #mutate(mean_age = mean(q50)) |>
   ungroup() |>
-  filter(n != 0, !is.na(mean_age))
+  filter(n != 0)
 
 
 age_diff_lu <-
@@ -1131,13 +1140,16 @@ depth_comp |> filter(year > 2003) |>
   #geom_histogram(alpha = 0.8) +
   geom_density(aes(group = year), alpha = 0.5) +
   #scale_fill_manual(values = survey_cols) +
-  facet_wrap(~ survey_abbrev, scales = 'free') +
+  facet_wrap(~ survey_abbrev, scales = 'free_y', ncol = 1) +
   # geom_vline(xintercept = c(min(mssm_dat$depth_m, na.rm = TRUE), max(mssm_dat$depth_m, na.rm = TRUE)))
-  geom_rect(data = tibble(survey_abbrev = 'SYN WCVI', depth_m = min(mssm_dat$depth_m, na.rm = TRUE)), aes(xmin = -Inf, xmax = depth_m, ymin = -Inf, ymax = Inf), fill = 'grey50', alpha = 0.1) +
-  geom_rect(data = tibble(survey_abbrev = 'SYN WCVI', depth_m = max(mssm_dat$depth_m, na.rm = TRUE)), aes(xmax = Inf, xmin = depth_m, ymin = -Inf, ymax = Inf), fill = 'grey50', alpha = 0.1) +
-  theme(axis.text.y = element_blank()) +
-  labs(x = "Depth (m)", y = "Sampling frequency")
-ggsave(file.path(mssm_figs, 'size-survey-correlation.png'), width = 7.6, height = 2.7)
+  # geom_rect(data = tibble(survey_abbrev = 'SYN WCVI', depth_m = min(mssm_dat$depth_m, na.rm = TRUE)), aes(xmin = -Inf, xmax = depth_m, ymin = -Inf, ymax = Inf), fill = 'grey50', alpha = 0.1) +
+  # geom_rect(data = tibble(survey_abbrev = 'SYN WCVI', depth_m = max(mssm_dat$depth_m, na.rm = TRUE)), aes(xmax = Inf, xmin = depth_m, ymin = -Inf, ymax = Inf), fill = 'grey50', alpha = 0.1) +
+  theme(axis.text.y = element_blank(),
+        legend.position = c(0.7, 0.9), legend.direction = 'horizontal',
+        legend.key.width=unit(0.05,"npc")) +
+  labs(x = "Depth (m)", y = "Sampling frequency", fill = "Year")
+
+ggsave(file.path(mssm_figs, 'depth-ranges-mssm-syn-wcvi.png'), width = 6.5, height = 4)
 
 
 
@@ -1199,21 +1211,18 @@ more_spp %>% filter(catch_weight == 0 & catch_count == 0) |>
 
 
 more_spp_summ <- more_spp |>
-  group_by(species_common_name) |>
-  summarise(mean_catch = mean(catch_weight, na.rm = TRUE)) |>
-  distinct(species_common_name)
+  group_by(species_common_name, species_science_name, year) |>
+  summarise(mean_catch = mean(catch_weight, na.rm = TRUE), .groups = 'drop') |>
+  mutate(parent_taxonomic_unit = species_science_name, parent_taxonomic_unit)
 
-spp_group_df <- bind_rows(mssm_dat, more_spp) |>
+
+spp_group_df <- mssm_dat |>
   group_by(species_common_name, species_science_name, year) |>
   summarise(mean_catch = mean(catch_weight, na.rm = TRUE), .groups = 'drop') |>
   left_join(gfsynopsis::get_spp_names()) |>
   mutate(parent_taxonomic_unit = ifelse(species_common_name == "pacific halibut", "pleuronectidae(righteye flounders)", parent_taxonomic_unit)) |>
   mutate(parent_taxonomic_unit = ifelse(species_common_name == "pacific hake", "merlucciidae", parent_taxonomic_unit)) |>
   mutate(parent_taxonomic_unit = ifelse(is.na(parent_taxonomic_unit), species_science_name, parent_taxonomic_unit))
-
-spp_group_df |>
-  distinct(species_common_name, parent_taxonomic_unit) |>
-  arrange(parent_taxonomic_unit)
 
 # Baseplot
 agg_plot <- function(df, ncol = 1, scales = 'free_y') {
@@ -1228,13 +1237,87 @@ agg_plot <- function(df, ncol = 1, scales = 'free_y') {
 
 # All higher order species aggregations of families caught in MSSM
 spp_group_plot <-
-  spp_group_df |>
+  more_spp_summ |>
   filter(species_common_name %in% c('eelpouts', 'flatfishes', 'rockfishes', 'sculpins', 'skates')) |>
   mutate(species_common_name = str_to_title(species_common_name)) |>
 agg_plot(ncol = 3)
 spp_group_plot
 
 ggsave(spp_group_plot, filename = file.path(mssm_figs, 'aggregated-spp-plot.png'), width = 8, height = 4)
+
+#
+plot_combined_spp <- function(family, common_name) {
+  df1 <- spp_group_df |>
+    filter(str_detect(parent_taxonomic_unit, family)) |>
+    group_by(year) |>
+    summarise(mean_catch = mean(mean_catch), .groups = 'drop') |>
+    mutate(species_common_name = 'species combined')
+
+  df2 <- more_spp_summ |>
+    filter(str_detect(parent_taxonomic_unit, family)) |>
+    group_by(year) |>
+    summarise(mean_catch = mean(mean_catch), .groups = 'drop') |>
+    mutate(species_common_name = common_name)
+
+  dat <- bind_rows(df1, df2)
+
+  ggplot(data = dat) +
+    geom_rect(aes(xmin = -Inf, xmax = 2003, ymin = -Inf, ymax = Inf),
+            fill = "gray90", alpha = 0.1) +
+    geom_vline(xintercept = 2001, colour = 'grey50') +
+    geom_point(aes(x = year, y = mean_catch, colour = species_common_name)) +
+    scale_colour_brewer(palette = "Dark2", type = 'qual') +
+    labs(x =  "Year", y = "Mean annual catch (kg)")
+}
+
+add_group_common_name <- function(df) {
+  df |>
+  mutate(group_common_name = case_when(
+      str_detect(parent_taxonomic_unit, "cottidae") ~ 'sculpins',
+      str_detect(parent_taxonomic_unit, "pleuronect|paralich") ~ 'flatfishes',
+      str_detect(parent_taxonomic_unit, "rajidae") ~ 'skates',
+      str_detect(parent_taxonomic_unit, "sebastes") ~ 'rockfishes',
+      str_detect(parent_taxonomic_unit, "zoarcidae") ~ 'eelpouts'))
+}
+
+plot_combined_spp('cottidae', "'sculpins'")
+plot_combined_spp('pleuronect|paralich', "'flatfishes'")
+plot_combined_spp('rajidae', "'skates'")
+plot_combined_spp('sebastes', "'rockfishes'")
+plot_combined_spp('zoarcidae', "'eelpouts'")
+
+df1 <- spp_group_df |>
+  filter(str_detect(parent_taxonomic_unit, c("cottidae|pleuronect|paralich|rajidae|sebastes|zoarcidae"))) |>
+  add_group_common_name() |>
+  group_by(group_common_name, year) |>
+  summarise(mean_catch = mean(mean_catch), .groups = 'drop') |>
+  mutate(species_common_name = 'Species')
+
+df2 <- more_spp_summ |>
+  filter(str_detect(parent_taxonomic_unit, c("cottidae|pleuronect|paralich|rajidae|sebastes|zoarcidae"))) |>
+  add_group_common_name() |>
+  group_by(group_common_name, year) |>
+  summarise(mean_catch = mean(mean_catch), .groups = 'drop') |>
+  mutate(species_common_name = 'Order/Family')
+
+dat <- bind_rows(df1, df2) |>
+  mutate(group_common_name = str_to_title(group_common_name))
+
+spp_group_plot2 <-
+  ggplot(data = dat, aes(x = year, y = mean_catch, colour = species_common_name, shape = species_common_name)) +
+    geom_rect(aes(xmin = -Inf, xmax = 2003, ymin = -Inf, ymax = Inf),
+            fill = "gray90", colour = NA, alpha = 0.1) +
+    geom_vline(xintercept = 2001, colour = 'grey50') +
+    geom_point() +
+    geom_line(linewidth = 0.3) +
+    scale_colour_brewer(palette = "Dark2", type = 'qual') +
+    labs(x =  "Year", y = "Mean annual catch (kg)", shape = "Identification level", colour = 'Identification level') +
+    facet_wrap(~ group_common_name, scales = "free_y", ncol = 2) +
+    theme(legend.position = c(0.7, 0.15))
+spp_group_plot2
+
+ggsave(spp_group_plot, filename = file.path(mssm_figs, 'aggregated-id-level-plot.png'), width = 7.7, height = 5.2)
+
 
 
 # Sculpins ---
@@ -1314,8 +1397,6 @@ design <- "
 "
 p_sculpin + p_skate + plot_layout(design = design)
 ggsave(filename = file.path(mssm_figs, 'agg-sculpin-skate.png'), width = 7, height = 7)
-
-future::plan(future::sequential)
 
 optimize_png <- TRUE
 if (optimize_png) {
