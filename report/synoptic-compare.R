47969a50273a8777b67ab9bc96559da47baf304d
Sean Anderson <sean@seananderson.ca>
2023-11-24 10:50:47 -0800

    Compare

--- report/synoptic-compare.R
+++ report/synoptic-compare.R
@@ -2,7 +2,7 @@ library(dplyr)
 library(ggplot2)
 theme_set(gfplot::theme_pbs())
 
-get_indexes <- function(folder, .family = "delta_gamma", model_tag = "st-rw") {
+get_indexes <- function(folder, .family = "delta-gamma", model_tag = "st-rw") {
   paths <- list.files(folder, pattern = ".rds", full.names = TRUE)
   file_names <- list.files(folder, pattern = ".rds")
   fits <- list.files(paste0(folder, "fits/"), pattern = ".rds", full.names = TRUE)
@@ -26,19 +26,83 @@ get_indexes <- function(folder, .family = "delta_gamma", model_tag = "st-rw") {
 }
 
 df_dg <- get_indexes("report/stitch-cache/synoptic-delta-gamma/", "delta-gamma")
-df_tw <- get_indexes("report/stitch-cache/synoptic/", "tweedie")
-df <- bind_rows(df_dg, df_tw)
+df_tw <- get_indexes("report/stitch-cache/synoptic-tweedie/", "tweedie")
+df_dpg <- get_indexes("report/stitch-cache/synoptic-delta-poisson-link-gamma/", "delta-poisson-link-gamma")
+df_dpl <- get_indexes("report/stitch-cache/synoptic-delta-poisson-link-lognormal/", "delta-poisson-link-lognormal")
+df_dl <- get_indexes("report/stitch-cache/synoptic-delta-lognormal/", "delta-lognormal")
+df <- bind_rows(df_dg, df_tw, df_dpl, df_dpg, df_dl)
 
 df |>
-  filter(!is.na(aic)) |>
+  # filter(family %in% c("delta-gamma", "tweedie")) |>
   group_by(species, family) |>
   summarise(aic = aic[1]) |>
   group_by(species) |>
-  summarise(delta_aic = aic[family == "delta-gamma"] - aic[family == "tweedie"]) |>
+  View()
+
+
+df |>
+  # filter(family %in% c("delta-gamma", "tweedie")) |>
+  group_by(species, family) |>
+  summarise(aic = aic[1]) |>
+  group_by(species) |>
+  reframe(delta_aic = aic[family == "delta-gamma"] - aic[family == "tweedie"]) |>
   ggplot(aes(delta_aic, species)) + geom_point() +
+  geom_vline(xintercept = 0, lty = 2)
+
+df |>
+  group_by(species, family) |>
+  summarise(aic = aic[1]) |>
+  group_by(species) |>
+  reframe(delta_aic = aic[family == "delta-gamma"] - aic[family == "delta-poisson-link-gamma"]) |>
+  ggplot(aes(delta_aic, species)) + geom_point() +
+  geom_vline(xintercept = 0, lty = 2)
+
+f <- df |>
+  group_by(species) |>
+  filter(year == min(year)) |>
+  reframe(lowest_aic = family[aic == min(aic)])
+
+table(f$lowest_aic)
+
+df |>
+  group_by(species, family) |>
+  summarise(aic = aic[1]) |>
+  group_by(species) |>
+  mutate(delta_aic = aic - min(aic)) |>
+  ggplot(aes(delta_aic + 1, species, colour = family)) + geom_point() +
+  scale_x_log10()
+  # geom_vline(xintercept = 0, lty = 2)
+
+df |>
+  group_by(species, family) |>
+  summarise(mean_cv = mean(mean_cv)) |>
+  ggplot(aes(mean_cv, species, colour = family)) + geom_point() +
+  geom_vline(xintercept = 0, lty = 2)
+
+df |>
+  group_by(species, family) |>
+  summarise(mean_cv = mean_cv[1], aic = aic[1]) |>
+  group_by(species) |>
+  mutate(scaled_mean_cv = scale(mean_cv), scaled_aic = scale(aic)) |>
+  ggplot(aes(scaled_mean_cv, scaled_aic, colour = family)) + geom_point() +
   geom_vline(xintercept = 0, lty = 2)
 
 ggplot(df, aes(year, biomass, ymin = lowerci, ymax = upperci, fill = family)) +
   geom_line(aes(colour = family)) + geom_ribbon(alpha = 0.5) +
   facet_wrap(~species, scales = "free_y")
 
+f_lu <- df |>
+  group_by(species) |>
+  filter(year == min(year)) |>
+  reframe(family = family[aic == min(aic)])
+
+df |>
+  right_join(f_lu) |>
+  ggplot(aes(year, biomass, ymin = lowerci, ymax = upperci, fill = family)) +
+  geom_line(aes(colour = family)) + geom_ribbon(alpha = 0.5) +
+  facet_wrap(~species, scales = "free_y")
+
+lu <- expand.grid(family = unique(df$family), species = unique(df$species))
+
+didnt_converge <- anti_join(lu, df) |> left_join(f)
+didnt_converge
