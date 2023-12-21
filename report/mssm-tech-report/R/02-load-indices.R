ce27745712ebc3a96a52285d5b14837101e3e7a7
Jillian Dunic <>
2023-12-21 12:34:25 -0800

    fix scaling to improve visual clarity

--- report/mssm-tech-report/R/02-load-indices.R
+++ report/mssm-tech-report/R/02-load-indices.R
@@ -5,6 +5,7 @@ if (!('mssm_loaded' %in% ls())) {
 # Load index dataframes ----
 
 # MSSM design index
+if (!file.exists(file.path(mssm_data, 'mssm-design-inds.rds'))) {
 mssm_d_inds <- spp_vector |>
   map(\(sp) readRDS(file.path(data_cache, paste0(gfsynopsis:::clean_name(sp), '.rds')))$survey_index) |>
   setNames(spp_vector) |>
@@ -12,7 +13,21 @@ mssm_d_inds <- spp_vector |>
   as_tibble() |>
   filter(survey_abbrev == 'MSSM WCVI') |>
   mutate(survey_abbrev = "MSSM Design")
+  #saveRDS(mssm_d_inds, file = file.path(mssm_data, 'mssm-design-inds.rds'))
+} else {
+  mssm_d_inds <- readRDS(file.path(mssm_data, 'mssm-design-inds.rds')) |>
+    group_by(species) |>
+    mutate(mean_cv = mean(re, na.rm = TRUE)) |>
+    ungroup()
+}
 
+# 2023 data
+# mssm_d_inds <- readRDS(file.path(mssm_data, 'mssm-index-dat.rds')) |>
+#     rename(species = 'species_common_name') |>
+#     mutate(survey_abbrev = "MSSM Design") |>
+#     group_by(species) |>
+#     mutate(mean_cv = mean(re, na.rm = TRUE)) |>
+#     ungroup()
 
 # Load SYN WCVI and MSSM best models
 get_index <- function(folder, spp, .family = "", model_tag = "st-rw") {
@@ -47,28 +62,36 @@ take_min_aic <- function(x) {
   }
 }
 
-syn_wcvi <- tidyr::expand_grid(.s = gfsynopsis::clean_name(spp_vector), f = families) |>
+syn_wcvi_inds <- tidyr::expand_grid(.s = gfsynopsis::clean_name(spp_vector), f = families) |>
   purrr::pmap_dfr(function(.s, f) {
   get_index(paste0("report/stitch-cache/synoptic-SYN WCVI-", f, "/"), .s, .family = f)
 }) |> group_by(species) |>
-  take_min_aic()
+  take_min_aic() |>
+  ungroup() |>
+  mutate(grid = 'SYN WCVI')
 
-syn_wcvi$species <- gsub("rougheye blackspotted", "rougheye/blackspotted", syn_wcvi$species)
+syn_wcvi_inds$species <- gsub("rougheye blackspotted", "rougheye/blackspotted", syn_wcvi_inds$species)
 
-syn_mssm_grid_inds <- syn_wcvi |>
+syn_mssm_grid_inds <- syn_wcvi_inds |>
   select(.sp = species, .family = family) |>
   distinct() |>
   purrr::pmap(\(.sp, .family) {
     get_index(paste0("report/stitch-cache/synoptic-mssm-SYN WCVI-", .family, "/"), clean_name(.sp), .family = .family)
   }) |>
   bind_rows() |>
-  as_tibble()
+  as_tibble() |>
+  mutate(survey_abbrev = 'SYN WCVI on MSSM Grid') |>
+  mutate(grid = 'MSSM 3km')
+
 
 mssm_inds <- tidyr::expand_grid(.s = gfsynopsis::clean_name(spp_vector), f = families) |>
   purrr::pmap_dfr(function(.s, f) {
   get_index(paste0("report/stitch-cache/mssm-", f, "/"), .s, .family = f)
 }) |> group_by(species) |>
-  take_min_aic()
+  take_min_aic() |>
+  ungroup() |>
+  mutate(survey_abbrev = "MSSM Model") |>
+  mutate(extreme_uci = max(upperci) > 10 * max(biomass))
 
 # Old tweedie MSSM indexes comparing 2km and 3km
 mssm_2km_inds <- spp_vector |>
@@ -95,24 +118,24 @@ mssm_3km_inds <- spp_vector |>
   ungroup()
 
 # SYN WCVI index
-syn_inds <-
-  spp_vector |>
-    map(\(sp) readRDS(file.path(syn_sc, 'syn-wcvi-grid', paste0(gfsynopsis:::clean_name(sp), '_st-rw.rds')))) |>
-    setNames(spp_vector) |>
-    keep(\(x) inherits(x, 'data.frame')) |>
-    bind_rows(.id = 'species') |>
-    mutate(grid = 'SYN WCVI') |>
-    as_tibble()
+# syn_inds <-
+#   spp_vector |>
+#     map(\(sp) readRDS(file.path(syn_sc, 'syn-wcvi-grid', paste0(gfsynopsis:::clean_name(sp), '_st-rw.rds')))) |>
+#     setNames(spp_vector) |>
+#     keep(\(x) inherits(x, 'data.frame')) |>
+#     bind_rows(.id = 'species') |>
+#     mutate(grid = 'SYN WCVI') |>
+#     as_tibble()
 
-syn_mssm_grid_inds <-
-  spp_vector |>
-    map(\(sp) readRDS(file.path(syn_sc, 'mssm-grid-3km', paste0(gfsynopsis:::clean_name(sp), '_st-rw.rds')))) |>
-    setNames(spp_vector) |>
-    keep(\(x) inherits(x, 'data.frame')) |>
-    bind_rows(.id = 'species') |>
-    as_tibble() |>
-    mutate(survey_abbrev = 'SYN WCVI on MSSM Grid') |>
-    mutate(grid = "3km MSSM")
+# syn_mssm_grid_inds <-
+#   spp_vector |>
+#     map(\(sp) readRDS(file.path(syn_sc, 'mssm-grid-3km', paste0(gfsynopsis:::clean_name(sp), '_st-rw.rds')))) |>
+#     setNames(spp_vector) |>
+#     keep(\(x) inherits(x, 'data.frame')) |>
+#     bind_rows(.id = 'species') |>
+#     as_tibble() |>
+#     mutate(survey_abbrev = 'SYN WCVI on MSSM Grid') |>
+#     mutate(grid = "3km MSSM")
 
 # --- CPUE index ---
 cpue_ind <- spp_vector |>
@@ -130,41 +153,26 @@ syn_years <- unique(sw_dat$year)
 mssm_years <- unique(mssm_dat$year)
 cpue_years <- unique(cpue_ind$year)
 
-#spp_in_mssm <- unique(mssm_3km_inds$species)
-spp_in_mssm <- mssm_3km_inds |>
-  filter(!extreme_uci | is.na(extreme_uci)) |>
-  filter(mean_cv < 4 | is.na(mean_cv)) |>
-  distinct(species) |>
-  pluck('species')
-
-spp_in_mssm_design <- mssm_d_inds |>
-  count(species) |>
-  filter(n > 2) |>
-  distinct(species) |>
-  pluck('species')
-
-spp_in_mssm_design_only <- setdiff(spp_in_mssm_design, spp_in_mssm)
-
 syn_mssm_overlap <- intersect(syn_years, mssm_years)
 cpue_mssm_overlap <- intersect(cpue_years, mssm_years)
 
 # MSSM with SYN WCVI - Use 3km grid
-inds <- bind_rows(mssm_3km_inds, syn_inds, syn_mssm_grid_inds, cpue_ind, mssm_d_inds) |>
+inds <- bind_rows(mssm_inds, syn_wcvi_inds, syn_mssm_grid_inds, cpue_ind, mssm_d_inds) |>
   mutate(syn_overlap = ifelse(year %in% syn_mssm_overlap, TRUE, FALSE),
          cpue_overlap = ifelse(year %in% cpue_mssm_overlap, TRUE, FALSE))
 
 mssm_geomeans <- inds |>
-  filter(survey_abbrev %in% c("MSSM WCVI", "MSSM Design")) |>
+  filter(survey_abbrev %in% c("MSSM Model", "MSSM Design")) |>
   group_by(species, survey_abbrev) |>
   summarise(mssm_geomean = exp(mean(log(biomass))), .groups = 'drop')
 
 syn_overlap_geomeans <- inds |>
-  filter(syn_overlap, survey_abbrev %in% c("MSSM WCVI", "SYN WCVI", "SYN WCVI on MSSM Grid")) |>
+  filter(syn_overlap, survey_abbrev %in% c("MSSM Model", "SYN WCVI", "SYN WCVI on MSSM Grid")) |>
   group_by(species, survey_abbrev) |>
   summarise(syn_overlap_geomean = exp(mean(log(biomass))), .groups = 'drop')
 
 cpue_overlap_geomeans <- inds |>
-  filter(cpue_overlap, survey_abbrev %in% c("MSSM WCVI", "CPUE 3CD")) |>
+  filter(cpue_overlap, survey_abbrev %in% c("MSSM Model", "CPUE 3CD")) |>
   group_by(species, survey_abbrev) |>
   summarise(cpue_overlap_geomean = exp(mean(log(biomass))), .groups = 'drop')
 
@@ -181,8 +189,103 @@ scaled_inds <- left_join(inds, syn_overlap_geomeans) |>
          mssm_scaled_lowerci = lowerci / mssm_geomean,
          mssm_scaled_upperci = upperci / mssm_geomean,
        ) |>
-  mutate(survey_abbrev = gsub("MSSM WCVI", "MSSM Model", survey_abbrev)) |>
+  mutate(species = gsub("rougheye blackspotted", "rougheye/blackspotted", species)) |>
   order_spp()
+
+raw_inds <- bind_rows(mssm_inds, syn_wcvi_inds, syn_mssm_grid_inds, cpue_ind, mssm_d_inds) |>
+  mutate(syn_overlap = ifelse(year %in% syn_mssm_overlap, TRUE, FALSE),
+         cpue_overlap = ifelse(year %in% cpue_mssm_overlap, TRUE, FALSE)) |>
+  mutate(survey_abbrev = factor(survey_abbrev, levels = names(survey_cols))) |>
+  mutate(species = gsub("rougheye blackspotted", "rougheye/blackspotted", species)) |>
+  order_spp()
+
+spp_in_mssm <- mssm_inds |>
+  filter(!extreme_uci | is.na(extreme_uci)) |>
+  filter(mean_cv < 4 | is.na(mean_cv)) |>
+  distinct(species) |>
+  mutate(species = gsub("rougheye blackspotted", "rougheye/blackspotted", species)) |>
+  pluck('species') |>
+  stringr::str_to_title()
+
+spp_in_mssm_design <- mssm_d_inds |>
+  count(species) |>
+  filter(n > 2) |>
+  distinct(species) |>
+  pluck('species') |>
+  stringr::str_to_title()
+
+spp_in_mssm_design_only <- setdiff(spp_in_mssm_design, spp_in_mssm)
+
+scale_geo_design <- function(df, survey1, survey2) {
+  df <- filter(df, survey_abbrev %in% c(survey1, survey2))
+
+  lvls <- levels(df$survey_abbrev)
+  both_scaled <- group_by(df, species) |>
+    group_split() |>
+    purrr::map_dfr(\(x) {
+      both_present <- length(unique(x$survey_abbrev[!is.na(x$biomass)])) > 1L
+      if (both_present) {
+        x_ind1 <- filter(x, survey_abbrev == survey1)
+        x_ind2 <- filter(x, survey_abbrev == survey2)
+
+        xx <- inner_join(
+          select(x_ind1, year),
+          select(x_ind2, year, biomass),
+          by = join_by(year)
+        ) |>
+          filter(biomass > 0) # can't take geometric mean of these!
+        overlapping_non_zero_years <- xx$year
+        x_geo_mean <- exp(mean(log(x_ind1$biomass[x_ind1$year %in% overlapping_non_zero_years])))
+        x_des_mean <- exp(mean(log(x_ind2$biomass[x_ind2$year %in% overlapping_non_zero_years])))
+
+        x_ind1 <- mutate(x_ind1,
+          biomass_scaled = biomass / x_geo_mean,
+          lowerci_scaled = lowerci / x_geo_mean,
+          upperci_scaled = upperci / x_geo_mean
+        )
+        x_ind2 <- mutate(x_ind2,
+          biomass_scaled = biomass / x_des_mean,
+          lowerci_scaled = lowerci / x_des_mean,
+          upperci_scaled = upperci / x_des_mean
+        )
+
+        max_geo <- max(x_ind1$upperci_scaled, na.rm = TRUE)
+
+        xx <- bind_rows(x_ind1, x_ind2)
+        mutate(xx,
+          biomass_scaled = biomass_scaled / max_geo,
+          lowerci_scaled = lowerci_scaled / max_geo,
+          upperci_scaled = upperci_scaled / max_geo
+        )
+      } else {
+        if (sum(!is.na(x$biomass))) {
+          mutate(x,
+            biomass_scaled = biomass / max(upperci, na.rm = TRUE),
+            lowerci_scaled = lowerci / max(upperci, na.rm = TRUE),
+            upperci_scaled = upperci / max(upperci, na.rm = TRUE)
+          )
+        } else {
+          mutate(x,
+            biomass_scaled = NA_real_,
+            lowerci_scaled = NA_real_,
+            upperci_scaled = NA_real_
+          )
+        }
+      }
+    }) |>
+    #mutate(biomass = biomass_scaled, lowerci = lowerci_scaled, upperci = upperci_scaled) |>
+    mutate(survey_abbrev = factor(survey_abbrev, levels = lvls)) |>
+    mutate(comp = paste(survey1, survey2, sep = '-'))
+}
+
+max_ci_scaled <- tibble(
+  'survey1' = rep('MSSM Model', 4),
+  'survey2' = c('MSSM Design', 'SYN WCVI', 'SYN WCVI on MSSM Grid', 'CPUE 3CD')) |>
+purrr::pmap(\(survey1, survey2) scale_geo_design(raw_inds |> filter(species %in% spp_in_mssm),
+  survey1  = 'MSSM Model', survey2 = 'SYN WCVI')
+) |>
+bind_rows()
+
 
 indices_loaded <- TRUE
 
--- report/mssm-tech-report/R/03-compare-indices.R
+++ report/mssm-tech-report/R/03-compare-indices.R
@@ -1,96 +1,234 @@
-if (!indices_loaded) {
-  source(here::here('report', 'mssm-tech-report', '02-load-indices.R'))
+if (!('indices_loaded' %in% ls())) {
+  source(here::here('report', 'mssm-tech-report', 'R', '02-load-indices.R'))
 }
 
-# Compare 2km and 3km grid indices
-grid_bin_ind_plot <- bind_rows(mssm_2km_inds, mssm_3km_inds) |>
-  order_spp() |>
-  ggplot(data = _, aes(x = year, y = biomass)) +
-    geom_line(aes(colour = grid)) +
-    geom_point(aes(colour = grid)) +
-    geom_ribbon(aes(ymin = lowerci, ymax = upperci, fill = grid), alpha = 0.3) +
-    geom_rect(data = . %>% filter(extreme_uci == TRUE | mean_cv > 2) %>%
-      distinct(species, grid, .keep_all = TRUE),
-      mapping = aes(xmin = -Inf, xmax = Inf, ymin = -Inf, ymax = Inf),
-      fill = "gray85", alpha = 0.3) +
-    scale_color_brewer(palette = "Dark2") +
-    scale_fill_brewer(palette = "Dark2" ) +
-    facet_wrap(~ species, scales = "free_y", nrow = 6) +
-    #ggtitle("Comparing modeled index from 2x2 km and 3x3 km grids") +
-    theme(legend.position = c(0.5, 1.03),
-          axis.text.y = element_blank()) +
-    guides(color = guide_legend(direction = "horizontal"), fill = guide_legend(direction = "horizontal")) +
-    labs(colour = "Model", fill = "Model") +
-    labs(x = 'Year', y = 'Relative biomass index')
-grid_bin_ind_plot
+tidy_stats_df <- function(df) {
+  select(df, species, survey_abbrev, mean_cv, num_sets, num_pos_sets) %>%
+    group_by(species, survey_abbrev) %>%
+    summarise(
+      mean_cv = sprintf("%.2f", round(mean(mean_cv, na.rm = TRUE), 2)),
+      n_pos_sets = sprintf("%.0f", round(mean(num_pos_sets, na.rm = TRUE), 0)),
+      n_sets = sprintf("%.0f", round(mean(num_sets, na.rm = TRUE), 0))) %>%
+    mutate(sets = paste0("Mean +ve sets", ": ", n_pos_sets, "/", n_sets)) %>%
+    mutate(cv = paste0("Mean", " CV: ", mean_cv)) %>%
+    mutate(cv = ifelse(mean_cv == "NaN", "", cv)) %>%
+    mutate(sets = ifelse(n_pos_sets == "NaN", "", sets)) #%>%
+    # mutate(mean_cv = as.numeric(mean_cv)) %>%
+    # mutate(n_pos_sets = as.numeric(n_pos_sets),
+    #   n_sets = as.numeric(n_sets))
+}
 
-ggsave(file.path(mssm_figs, '2km-3km-grid-model-comp.png'), plot = grid_bin_ind_plot,
-  width = 10.5, height = 8)
-
-# Compare MSSM and SYN WCVI and 3CD CPUE --------------------------------------
-
-ind_layers <- function(colours = survey_cols, ncol = 5) {
+ind_layers <- function(colours = survey_cols, ncol = 4,
+  xlim = c(1975 - 0.2, 2022 + 0.2), breaks = 10) {
   layers <- list(
-      geom_line(),
-      geom_point(),
+      show_grid_lines(xlim, breaks),
       scale_colour_manual(values = colours),
       scale_fill_manual(values = colours),
       facet_wrap(~ species, scale = 'free_y', ncol = ncol),
-      theme(legend.position = 'top',
-            legend.title = element_blank(),
-            axis.text.y = element_blank()),
-      guides(color = guide_legend(direction = "horizontal"), fill = guide_legend(direction = "horizontal")),
-      labs(x = 'Year', y = 'Relative biomass index')
-    )
+  theme(legend.position = 'top',
+        legend.title = element_blank(),
+        axis.text.y = element_blank(),
+        axis.ticks.y = element_blank(),
+        panel.spacing = unit(-0.1, "lines")
+        ),
+  guides(color = guide_legend(direction = "horizontal"),
+    fill = 'none'),
+  labs(x = 'Year', y = 'Relative biomass index'),
+  coord_cartesian(xlim = c(1975, 2022) + c(-0.75, 0.75),
+      ylim = c()
+    expand = FALSE)
+  )
 }
 
-mssm_design_only_inds <-
-  scaled_inds |>
+show_grid_lines <- function(xlim = c(1975 - 0.2, 2022 + 0.2), breaks = 10)
+  list(geom_vline(xintercept = seq(xlim[1], xlim[2]), col = "grey96", lwd = 0.3),
+       geom_vline(xintercept = seq(gfplot:::mround(xlim[1], breaks), xlim[2], breaks),
+        col = "grey93")
+     )
+
+show_text <- function(stat_df) {
+  list(
+    geom_text(data = stat_df, aes(label = cv),
+      x = 1975 + 0.5, y = 0.87,
+      colour = "grey35", size = 2.65, hjust = 0),
+    geom_text(data = stat_df, aes(label = sets),
+        x = 1975 + 0.5, y = 0.69,
+        colour = "grey35", size = 2.65, hjust = 0)
+  )
+}
+
+# Compare 2km and 3km grid indices
+# grid_bin_ind_plot <- bind_rows(mssm_2km_inds, mssm_3km_inds) |>
+#   order_spp() |>
+#   ggplot(data = _, aes(x = year, y = biomass)) +
+#     geom_line(aes(colour = grid)) +
+#     geom_point(aes(colour = grid)) +
+#     geom_ribbon(aes(ymin = lowerci, ymax = upperci, fill = grid), alpha = 0.3) +
+#     geom_rect(data = . %>% filter(extreme_uci == TRUE | mean_cv > 2) %>%
+#       distinct(species, grid, .keep_all = TRUE),
+#       mapping = aes(xmin = -Inf, xmax = Inf, ymin = -Inf, ymax = Inf),
+#       fill = "gray85", alpha = 0.3) +
+#     scale_color_brewer(palette = "Dark2") +
+#     scale_fill_brewer(palette = "Dark2" ) +
+#     facet_wrap(~ species, scales = "free_y", nrow = 6) +
+#     #ggtitle("Comparing modeled index from 2x2 km and 3x3 km grids") +
+#     theme(legend.position = c(0.5, 1.03),
+#           axis.text.y = element_blank()) +
+#     guides(color = guide_legend(direction = "horizontal"), fill = guide_legend(direction = "horizontal")) +
+#     labs(colour = "Model", fill = "Model") +
+#     labs(x = 'Year', y = 'Relative biomass index')
+# grid_bin_ind_plot
+
+# ggsave(file.path(mssm_figs, '2km-3km-grid-model-comp.png'), plot = grid_bin_ind_plot,
+#   width = 10.5, height = 8)
+
+# MSSM Design only ------------------------
+mssm_design_only_inds <- scaled_inds |>
   filter(species %in% spp_in_mssm_design_only,
          survey_abbrev %in% c("MSSM Design")) |>
 ggplot(data = _, aes(x = year, y = mssm_scaled_biomass, colour = survey_abbrev, fill = survey_abbrev)) +
-  geom_pointrange(aes(ymin = mssm_scaled_lowerci, ymax = mssm_scaled_upperci), size = 0.2) +
+  show_grid_lines() +
+  geom_rect(data = . %>% distinct(species, .keep_all = TRUE),
+      mapping = aes(xmin = -Inf, xmax = 2003, ymin = -Inf, ymax = Inf),
+      fill = "gray85", alpha = 0.3) +
+  geom_pointrange(aes(ymin = mssm_scaled_lowerci, ymax = mssm_scaled_upperci),
+    size = 0.2, alpha = 0.7, shape = 21, fill = 'grey95') +
   geom_ribbon(aes(ymin = mssm_scaled_lowerci, ymax = mssm_scaled_upperci), size = 0.2, alpha = 0.1, colour = NA) +
   scale_colour_manual(values = survey_cols) +
   scale_fill_manual(values = survey_cols) +
   facet_wrap(~ species, scale = 'free_y', ncol = 4) +
   theme(legend.position = c(0.5, 1.055),
-        legend.title = element_blank(),
-        axis.text.y = element_blank()) +
-  # geom_rect(data = . %>% distinct(species, .keep_all = TRUE),
-  #     mapping = aes(xmin = -Inf, xmax = 2003, ymin = -Inf, ymax = Inf),
-  #     fill = "gray85", alpha = 0.3) +
-  guides(colour = 'none', fill = 'none')
-  #ggtitle("Design based index only") +
+        axis.text.y = element_blank(),
+        legend.title = element_blank()
+        ) +
+  guides(colour = 'none', fill = 'none') +
+  labs(x = 'Year', y = 'Relative biomass index')
 mssm_design_only_inds
 
 ggsave(file.path(mssm_figs, 'index-mssm-design.png'), plot = mssm_design_only_inds,
   width = 10.5, height = 8)
 
-mssm_model_design_inds <- scaled_inds |>
+# MSSM Model - Design ------------------------
+# mssm_model_design_inds <- scaled_inds |>
+#   filter(species %in% spp_in_mssm,
+#          survey_abbrev %in% c("MSSM Model")) |>
+# ggplot(data = _, aes(x = year, y = mssm_scaled_biomass, colour = survey_abbrev, fill = survey_abbrev)) +
+#   ind_layers(ncol = 4) +
+#   geom_line(aes(colour = 'MSSM Model')) +
+#   geom_ribbon(aes(ymin = mssm_scaled_lowerci, ymax = mssm_scaled_upperci), colour = NA, alpha = 0.3) +
+#   geom_pointrange(data = scaled_inds |>
+#     filter(species %in% spp_in_mssm, survey_abbrev == "MSSM Design") |>
+#     filter(year != 2022),
+#     aes(ymin = mssm_scaled_lowerci, ymax = mssm_scaled_upperci),
+#     size = 0.2, alpha = 0.7, shape = 21, fill = 'grey95') +
+#   guides(shape = 'none')
+# mssm_model_design_inds
+
+# ggsave(file.path(mssm_figs, 'index-model-design.png'), plot = mssm_model_design_inds,
+#   width = 10.5, height = 10)
+
+# ---- Use what was done with plot-indices -----
+# Scale to max-CI of the modelled index and allow the design CIs to go off the plot
+scaled_mssm <- raw_inds |>
   filter(species %in% spp_in_mssm,
-         survey_abbrev %in% c("MSSM Model")) |>
-ggplot(data = _, aes(x = year, y = mssm_scaled_biomass, colour = survey_abbrev, fill = survey_abbrev)) +
-  geom_ribbon(aes(ymin = mssm_scaled_lowerci, ymax = mssm_scaled_upperci), colour = NA, alpha = 0.3) +
-  ind_layers() +
-  geom_pointrange(data = scaled_inds |> filter(species %in% spp_in_mssm, survey_abbrev == "MSSM Design"),
-    aes(ymin = mssm_scaled_lowerci, ymax = mssm_scaled_upperci), size = 0.2, alpha = 0.7)
-  #ggtitle("Comparison of Modelled and Design based index")
+    survey_abbrev %in% c('MSSM Model', 'MSSM Design')) |>
+  scale_geo_design(survey1  = 'MSSM Model', survey2 = 'MSSM Design')
+
+mssm_stats <- tidy_stats_df(scaled_mssm) |>
+  pivot_wider(id_cols = species,
+    names_from = 'survey_abbrev',
+    values_from = c('mean_cv', 'n_pos_sets', 'n_sets')) |>
+    mutate(sets = paste0("Mean +ve sets", ": ", `n_pos_sets_MSSM Model`, "/", `n_sets_MSSM Model`)) %>%
+    mutate(cv = paste0("Mean", " CV: ", `mean_cv_MSSM Model`, '(M), ', `mean_cv_MSSM Design`, '(D)'))
+
+mssm_model_design_inds <- ggplot() +
+  show_grid_lines() +
+  geom_line(data = scaled_mssm |>
+    filter(species %in% spp_in_mssm, survey_abbrev == "MSSM Model"),
+    aes(x = year, y = biomass_scaled, colour = 'MSSM Model')) +
+  geom_ribbon(data = scaled_mssm |>
+    filter(species %in% spp_in_mssm, survey_abbrev == "MSSM Model"),
+    aes(fill = 'MSSM Model', x = year, ymin = lowerci_scaled, ymax = upperci_scaled), colour = NA, alpha = 0.3) +
+  geom_pointrange(data = scaled_mssm |>
+    filter(species %in% spp_in_mssm, survey_abbrev == "MSSM Design"),
+    aes(x = year, y = biomass_scaled, ymin = lowerci_scaled, ymax = upperci_scaled, colour = 'MSSM Design'),
+    size = 0.2, alpha = 0.7, shape = 21, fill = 'grey95') +
+  scale_colour_manual(values = survey_cols) +
+  scale_fill_manual(values = survey_cols) +
+  facet_wrap(~ species, scale = 'free_y', ncol = 4) +
+  theme(legend.position = 'top',
+        legend.title = element_blank(),
+        axis.text.y = element_blank(),
+        axis.ticks.y = element_blank(),
+        panel.spacing = unit(-0.1, "lines")
+        ) +
+  guides(color = guide_legend(direction = "horizontal"),
+    fill = 'none') +
+  labs(x = 'Year', y = 'Relative biomass index') +
+  coord_cartesian(ylim = c(-0.004, 1.05), xlim = c(1975, 2022) + c(-0.75, 0.75),
+    expand = FALSE
+  ) +
+  show_text(mssm_stats)
 mssm_model_design_inds
 
-ggsave(file.path(mssm_figs, 'index-model-design.png'), plot = mssm_model_design_inds,
-  width = 10.5, height = 10)
+ggsave(file.path(mssm_figs, 'index-model-design_pjs-mode.png'),
+  width = 8, height = 11)
 
-mssm_syn_inds <- scaled_inds |>
+# ----
+# SYN WCVI vs MSSM
+stats_df <- tidy_stats_df(scaled_inds |>
   filter(species %in% spp_in_mssm,
-         survey_abbrev %in% c("MSSM Model", "SYN WCVI")) |>
-ggplot(data = _, aes(x = year, y = syn_scaled_biomass, colour = survey_abbrev, fill = survey_abbrev)) +
-  geom_ribbon(aes(ymin = syn_scaled_lowerci, ymax = syn_scaled_upperci), colour = NA, alpha = 0.3) +
+         survey_abbrev %in% c("MSSM Model", "SYN WCVI"))) |>
+  mutate(prop_pos = round(as.numeric(n_pos_sets) / as.numeric(n_sets), digits = 2)) |>
+  pivot_wider(id_cols = species,
+    names_from = 'survey_abbrev',
+    values_from = c('mean_cv', 'n_pos_sets', 'n_sets', 'prop_pos')) |>
+    mutate(sets = paste0("Mean +ve sets", ": ", `prop_pos_MSSM Model`, '(M), ', `prop_pos_SYN WCVI`, '(S)')) %>%
+    mutate(cv = paste0("Mean", " CV: ", `mean_cv_MSSM Model`, '(M), ', `mean_cv_SYN WCVI`, '(S)'))
+
+test <- raw_inds |>
+  filter(species %in% spp_in_mssm,
+    survey_abbrev %in% c('MSSM Model', 'SYN WCVI')) |>
+  scale_geo_design(survey1  = 'MSSM Model', survey2 = 'SYN WCVI')
+
+
+mssm_syn_inds <- max_ci_scaled |>
+  filter(comp == c("MSSM Model-SYN WCVI")) |>
+ggplot(data = _) +
   ind_layers() +
-geom_rect(data = . %>% filter(!(species %in% unique(syn_inds$species))) %>%
+  geom_line(aes(x = year, y = biomass_scaled, colour = survey_abbrev)) +
+  geom_ribbon(aes(x = year, ymin = lowerci_scaled, ymax = upperci_scaled, fill = survey_abbrev),
+    colour = NA, alpha = 0.3) +
+  show_text(stats_df) +
+  geom_rect(data = . %>% filter(!(tolower(species) %in% unique(syn_wcvi_inds$species))) %>%
     distinct(species, .keep_all = TRUE),
     mapping = aes(xmin = -Inf, xmax = Inf, ymin = -Inf, ymax = Inf),
-    fill = "white", colour = NA)
+    fill = "white", colour = NA) +
+  ylim(c(-0.004, 1.03))
+mssm_syn_inds
+
+
+mssm_syn_inds <-
+
+scaled_inds |>
+  filter(species %in% spp_in_mssm,
+         survey_abbrev %in% c("MSSM Model", "SYN WCVI")) |>
+ggplot(data = _, aes(x = year, y = syn_scaled_biomass)) +
+  ind_layers() +
+  geom_line(aes(colour = survey_abbrev)) +
+  geom_ribbon(aes(ymin = syn_scaled_lowerci, ymax = syn_scaled_upperci, fill = survey_abbrev), colour = NA, alpha = 0.3) +
+  geom_rect(data = . %>% filter(!(tolower(species) %in% unique(syn_wcvi_inds$species))) %>%
+    distinct(species, .keep_all = TRUE),
+    mapping = aes(xmin = -Inf, xmax = Inf, ymin = -Inf, ymax = Inf),
+    fill = "white", colour = NA) +
+      geom_text(data = stats_df, aes(label = cv),
+      x = 1975 + 0.5, y = 0.67,
+      colour = "grey35", size = 2.65, hjust = 0)+
+    geom_text(data = stats_df, aes(label = sets),
+        x = 1975 + 0.5, y = 0.49,
+        colour = "grey35", size = 2.65, hjust = 0)
+
+
 mssm_syn_inds
 
 ggsave(file.path(mssm_figs, 'index-mssm-model-syn-wcvi-model.png'), plot = mssm_syn_inds,
@@ -100,13 +238,15 @@ mssm_syn_inds_mssm_grid <- scaled_inds |>
   filter(species %in% spp_in_mssm,
          survey_abbrev %in% c("MSSM Model", "SYN WCVI on MSSM Grid")) |>
 ggplot(data = _, aes(x = year, y = syn_scaled_biomass, colour = survey_abbrev, fill = survey_abbrev)) +
+  ind_layers(ncol = 4) +
+  geom_line() +
   geom_ribbon(aes(ymin = syn_scaled_lowerci, ymax = syn_scaled_upperci), colour = NA, alpha = 0.3) +
-  ind_layers() +
-  geom_rect(data = . %>% filter(!(species %in% unique(syn_inds$species))) %>%
+  geom_rect(data = . %>% filter(!(tolower(species) %in% unique(syn_wcvi_inds$species))) %>%
     distinct(species, .keep_all = TRUE),
     mapping = aes(xmin = -Inf, xmax = Inf, ymin = -Inf, ymax = Inf),
     fill = "white", colour = NA) +
   labs(x = 'Year', y = 'Relative biomass index')
+
 mssm_syn_inds_mssm_grid
 
 ggsave(file.path(mssm_figs, 'index-mssm-model-syn-wcvi-model-mssm-grid.png'), plot = mssm_syn_inds_mssm_grid,
