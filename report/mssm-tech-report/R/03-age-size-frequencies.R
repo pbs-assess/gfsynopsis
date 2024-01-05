9ef754b8b92000b2865fa9454abb818b0bf5ab32
Jillian Dunic <>
2024-01-05 11:55:06 -0800

    add raw data for discussion

--- report/mssm-tech-report/R/03-age-size-frequencies.R
+++ report/mssm-tech-report/R/03-age-size-frequencies.R
@@ -37,32 +37,50 @@ size_diff_lu <-
                           syn_minus_mssm_q50 < 0 ~ 'MSSM',
                           TRUE ~ "Equal")) |>
   arrange(bigger, abs_diff) |>
-  mutate(bg = ifelse(row_number() %% 2 == 1, 'grey95', NA))
+  mutate(bg = ifelse(row_number() %% 2 == 1, 'grey95', NA),
+         species = stringr::str_to_title(species_common_name)) |>
+  mutate(species = factor(species, levels = unique(species)))
 
 size_comp_df <-
   left_join(size_summary, size_diff_lu) |>
   group_by(species_common_name) |>
   filter(n() == 2) |> # get only species that are found in both surveys
   ungroup() |>
-  mutate(species_common_name = factor(species_common_name, levels = pluck(size_diff_lu, 'species_common_name'))) |>
-    arrange(species_common_name)
+  mutate(species = factor(species, levels = pull(size_diff_lu, species))) |>
+    arrange(species)
+
+raw_size_dat <- size_dat |>
+  left_join(size_diff_lu) |>
+  drop_na(abs_diff) |>
+  mutate(species = factor(species, levels = pull(size_diff_lu, species))) |>
+  distinct(species, survey_abbrev, year, length, .keep_all = TRUE)
+
 
 size_comp <- size_comp_df |>
-  ggplot(aes(x = species_common_name, y = q50, colour = survey_abbrev)) +
-    geom_tile(aes(height = Inf, width = 1, fill = bg), colour = NA, alpha = 0.3) +
+  ggplot(aes(x = species, y = q50, colour = survey_abbrev)) +
+  geom_point(data = raw_size_dat,
+      aes(x = species, y = length, fill = survey_abbrev),
+    colour = 'grey80', alpha = 0.2, shape = 21,
+    position = position_jitterdodge(dodge.width = 0.5,
+    jitter.height = 0.4, jitter.width = 0.2)
+    ) +
+#    geom_tile(aes(height = Inf, width = 1, fill = bg), colour = NA, alpha = 0.3) +
     geom_pointrange(mapping = aes(ymin = q25, ymax = q75),
        position = position_dodge(width = 0.4)) +
     scale_colour_manual(values = survey_cols) +
-    theme(axis.text.x = element_text(angle = 45, hjust = 1),
+    scale_fill_manual(values = c('#c3f5e6', '#d8d6e9'), guide = "legend") +
+    theme(axis.text.x = element_text(angle = 60, hjust = 1),
       axis.title.x = element_blank()) +
-    scale_fill_identity() +
+    #scale_fill_identity() +
+    guides(fill = 'none') +
     facet_grid(~ bigger, drop = TRUE, scales = 'free_x', space = 'free') +
     labs(colour = "Survey", y = 'Length (cm)') +
     theme(legend.position = c(0.5, -0.5), legend.direction = "horizontal")
 size_comp
 
 ggsave(file.path(mssm_figs, 'size-comp.png'), plot = size_comp,
-  width = 13, height = 4.5)
+  #width = 13, height = 4.5)
+  width = 10, height = 5.2)
 
 age_summary <- size_dat |>
   #filter(species_common_name %in% spp_in_mssm) |>
@@ -87,39 +105,54 @@ age_diff_lu <-
                           syn_minus_mssm_q50 < 0 ~ 'MSSM',
                           TRUE ~ "Equal")) |>
   arrange(bigger, abs_diff) |>
-  mutate(bg = ifelse(row_number() %% 2 == 1, 'grey95', NA)) |>
-  filter(!is.na(abs_diff))  # ignore species that are not measured in MSSM
+  mutate(bg = ifelse(row_number() %% 2 == 1, 'grey95', NA),
+         species = stringr::str_to_title(species_common_name)) |>
+  mutate(species = factor(species, levels = unique(species))) |>
+  drop_na(abs_diff)  # ignore species that are not measured in MSSM
+
+raw_age_dat <- size_dat |>
+  left_join(age_diff_lu) |>
+  drop_na(abs_diff) |>
+  mutate(species = factor(species, levels = pull(age_diff_lu, species))) |>
+  distinct(species, survey_abbrev, year, age, .keep_all = TRUE)
 
 age_comp <-
   left_join(age_summary, age_diff_lu) |>
     group_by(species_common_name) |>
     filter(n() > 1) |>
     ungroup() |>
-    mutate(species_common_name = factor(species_common_name, levels = pluck(age_diff_lu, 'species_common_name'))) |>
-    arrange(species_common_name) |>
-  ggplot(aes(x = species_common_name, y = q50, colour = survey_abbrev)) +
-    geom_tile(aes(height = Inf, width = 1, fill = bg), colour = NA, alpha = 0.3) +
+  ggplot(aes(x = species, y = q50, colour = survey_abbrev)) +
+    geom_point(data = raw_age_dat,
+      aes(x = species, y = age, fill = survey_abbrev),
+    colour = 'grey80', alpha = 0.2, shape = 21,
+    position = position_jitterdodge(dodge.width = 0.5,
+    jitter.height = 0.4, jitter.width = 0.2)
+    ) +
+    #geom_tile(aes(height = Inf, width = 1, fill = bg), colour = NA, alpha = 0.3) +
     geom_pointrange(mapping = aes(ymin = q25, ymax = q75),
        position = position_dodge(width = 0.4)) +
     scale_colour_manual(values = survey_cols) +
+    scale_fill_manual(values = c('#c3f5e6', '#d8d6e9'), guide = "legend") +
     theme(axis.text.x = element_text(angle = 45, hjust = 1),
       axis.title.x = element_blank()) +
-    scale_fill_identity() +
+    #scale_fill_identity() +
+    guides(fill = 'none') +
     facet_grid(~ bigger, drop = TRUE, scales = 'free_x', space = 'free') +
-    labs(colour = 'Survey', y = 'Age') +
-    theme(legend.position = "top")
+    labs(colour = 'Survey', y = 'Age') #+
+    #theme(legend.position = "top")
 age_comp
 
 ggsave(file.path(mssm_figs, 'age-comp.png'), plot = age_comp,
-  width = 4, height = 4)
+  width = 4.2, height = 4.5)
 
 # ------------------------------------------------------------------------------
 # ---- Look at size distributions at pulses --------
 #- scaled to geomean (2003 - 2022)
 plot_size_time <- function(sp, raw_data = FALSE) {
+  buffer <- 0.4
   p1 <- max_ci_scaled |>
-    filter(survey_abbrev %in% c("MSSM Model", "SYN WCVI on MSSM Grid")) |>
-    mutate(survey_abbrev = ifelse(survey_abbrev == "MSSM Model", "SYN WCVI", "MSSM WCVI")) |>
+    filter(comp %in% c("MSSM Model-SYN WCVI on MSSM Grid")) |>
+    mutate(survey_abbrev = ifelse(survey_abbrev == "MSSM Model", "MSSM WCVI", "SYN WCVI")) |>
     filter(year > 2003) |>
     filter(tolower(species) == sp) |>
     ggplot(aes(x = year, y = biomass_scaled, colour = survey_abbrev, fill = survey_abbrev)) +
@@ -129,12 +162,12 @@ plot_size_time <- function(sp, raw_data = FALSE) {
       scale_fill_manual(values = survey_cols, guide = "legend") +
       labs(x = 'Year', y = 'Relative biomass index', colour = "Survey", fill = "Survey") +
       ggtitle(paste0('MSSM Index - ', sp)) +
-      xlim(c(2003, 2022)) +
-      guides(colour = 'none', fill = 'none')
+      #guides(colour = 'none', fill = 'none')
+      coord_cartesian(xlim = c(2004 - buffer, 2022 + buffer), expand = FALSE)
 
   size_df <- size_dat |>
     filter(species_common_name == sp) |>
-    filter(year >= 2003)
+    filter(year > 2003)
 
   size_summ_df <- size_df |>
     group_by(species_common_name, year, survey_abbrev) |>
@@ -149,7 +182,7 @@ plot_size_time <- function(sp, raw_data = FALSE) {
   if (raw_data) {
     p2 <- p2 + geom_point(data = size_df, aes(x = year, y = length, fill = survey_abbrev),
       colour = 'grey80', alpha = 0.2, shape = 21,
-      position = position_jitterdodge(dodge.width = 0.4,
+      position = position_jitterdodge(dodge.width = 0.5,
         jitter.height = 0.4, jitter.width = 0.2)
     )
       #position = position_dodge(width = 0.4))
@@ -160,19 +193,20 @@ plot_size_time <- function(sp, raw_data = FALSE) {
     scale_colour_manual(values = survey_cols, guide = "legend") +
     scale_fill_manual(values = c('#c3f5e6', '#d8d6e9'), guide = "legend") +
     labs(x = "Year", y = 'Length (cm)', colour = "Survey") +
-    xlim(c(2003, 2022))
+    guides(fill = 'none') +
+    coord_cartesian(xlim = c(2004 - buffer, 2022 + buffer), expand = FALSE)
 
   (p1 / p2) + plot_layout(guides = "collect") & theme(legend.position = 'bottom')
 
 }
 
 sp <- 'eulachon'
+sp <- 'petrale sole'
 plot_size_time(sp, raw_data = TRUE)
 plot_size_time(sp, raw_data = FALSE)
 
 size_summary_spp <- distinct(size_comp_df, species_common_name) |>
-  pluck('species_common_name') |>
-  as.character()
+  pull(species_common_name)
 
 size_p_list <-
 size_summary_spp |>
