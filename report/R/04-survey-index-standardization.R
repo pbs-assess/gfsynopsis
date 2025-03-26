5e0918cfbbaf2e49ab23a5d810e36c6b44246334
Jillian Dunic <jdunic@gmail.com>
2025-03-26 10:50:38 -0700

    working index standardization summary output

--- report/R/04-survey-index-standardization.R
+++ report/R/04-survey-index-standardization.R
@@ -9,7 +9,6 @@ if (is.null(shapefile)) {
 dir.create(stitch_cache, showWarnings = FALSE, recursive = TRUE)
 
 # Stitch inputs
-model_type <- "st-rw"
 spp_vector <- spp$species_common_name[order(spp$species_common_name)]
 # randomize to avoid clumps of non-fitting in parallel:
 set.seed(92729)
@@ -17,6 +16,8 @@ spp_vector <- sample(spp_vector, length(spp_vector))
 # Synoptic/HBLL
 bait_counts <- readRDS(file.path(dc, "bait-counts.rds"))
 
+if (parallel_processing && is_hake_server()) future::plan(future::multicore, workers = cores)
+if (!is_hake_server()) future::plan(future::sequential)
 # Stitch surveys if not cached
 source(here("report", "run-stitching.R"))
-
+future::plan(sequential)
--- report/run-stitching.R
+++ report/run-stitching.R
@@ -162,13 +162,35 @@ furrr::future_walk(spp_vector, function(.sp) {
     check_cache = TRUE,
     shapefile = shapefile
   )
+
+ # IPHC FISS
+ # -----------------------------------------
+   iphc_dat <- gfdata::load_iphc_dat(species = .sp) |>
+     prep_iphc_stitch_dat(survey_dat = _)
+   get_stitched_index(
+     survey_dat = iphc_dat,
+     species = .sp,
+     survey_type = "IPHC FISS",
+     form = catch ~ 1,
+     family = "nb2",
+     time = 'year',
+     spatial = 'on',
+     spatiotemporal = 'rw',
+     use_extra_time = TRUE,
+     offset = 'offset',
+     gradient_thresh = 0.001,
+     cutoff = 20,
+     cache = sc_iphc,
+     check_cache = TRUE,
+     shapefile = shapefile
+   )
 })
 
 # Fit individual SYN surveys
 # -----------------------------------------
 tofit <- tidyr::expand_grid(.sp = spp_vector, .syn = syns, .family = families)
-# furrr::future_pmap(tofit, function(.sp, .syn, .family) {
-purrr::pmap(tofit, function(.sp, .syn, .family) {
+furrr::future_pmap(tofit, function(.sp, .syn, .family) {
+# purrr::pmap(tofit, function(.sp, .syn, .family) {
 
   .cache <- file.path(stitch_cache, paste0("synoptic-", .syn))
   spp_filename <- paste0(gfsynopsis:::clean_name(.sp), "_", .family, "_sp-on-st-rw", ".rds")
@@ -186,21 +208,21 @@ purrr::pmap(tofit, function(.sp, .syn, .family) {
     }
   }
   # RW individual SYN
-  get_stitched_index(
-    survey_dat = survey_dat,
-    species = .sp,
-    survey_type = .syn,
-    cutoff = .cutoff,
-    form = catch ~ 1,
-    family = .family,
-    spatial = "on",
-    spatiotemporal = "rw",
-    use_extra_time = TRUE,
-    offset = 'offset',
-    cache = .cache,
-    check_cache = TRUE,
-    shapefile = shapefile
-  )
+#  get_stitched_index(
+#    survey_dat = survey_dat,
+#    species = .sp,
+#    survey_type = .syn,
+#    cutoff = .cutoff,
+#    form = catch ~ 1,
+#    family = .family,
+#    spatial = "on",
+#    spatiotemporal = "rw",
+#    use_extra_time = TRUE,
+#    offset = 'offset',
+#    cache = .cache,
+#    check_cache = TRUE,
+#    shapefile = shapefile
+#  )
 
   # IID individual SYN
   get_stitched_index(
@@ -220,31 +242,6 @@ purrr::pmap(tofit, function(.sp, .syn, .family) {
   )
 })
 
-# IPHC FISS
-# -----------------------------------------
-# furrr::future_walk(spp_vector, function(.sp) {
-purrr::walk(spp_vector, function(.sp) {
-  survey_dat <- gfdata::load_iphc_dat(species = .sp) |>
-    prep_iphc_stitch_dat(survey_dat = _)
-  get_stitched_index(
-    survey_dat = survey_dat,
-    species = .sp,
-    survey_type = "IPHC FISS",
-    form = catch ~ 1,
-    family = "nb2",
-    time = 'year',
-    spatial = 'on',
-    spatiotemporal = 'rw',
-    use_extra_time = TRUE,
-    offset = 'offset',
-    gradient_thresh = 0.001,
-    cutoff = 20,
-    cache = sc_iphc,
-    check_cache = TRUE,
-    shapefile = shapefile
-  )
-})
-
 # ------------------------------------------------------------------------------
 # Get best model
 # -----------------------------------------
@@ -255,25 +252,37 @@ take_min_aic <- function(x) {
   }
 }
 
-# Question: I don't know if some should only be comparing iid, or compare it all?
+min_aic_dir <- file.path(stitch_cache, "min_aic")
+dir.create(min_aic_dir, showWarnings = FALSE, recursive = TRUE)
 sc_list <- list.files(stitch_cache)
-
+sc_list <- sc_list[!grepl("min_aic", sc_list)]
+single_regions <- c("hbll_outside_n", "hbll_outside_s",
+  paste0("synoptic-", syns)
+)
 purrr::map_dfr(spp_vector, function(.sp) {
-  sp_hyphens <- gfsynopsis::clean_name(spp_vector)
+  sp_hyphens <- gfsynopsis::clean_name(.sp)
   out_file <- file.path(min_aic_dir, paste0(sp_hyphens, ".rds"))
 
   ind_df <- purrr::map_dfr(sc_list, function(.sc) {
     cache_files <- list.files(file.path(stitch_cache, .sc), full.names = TRUE)
     index_files <- cache_files[grepl(sp_hyphens, cache_files)]
 
+    if (.sc %in% single_regions) { # compare only iid models for single regions
+      index_files <- index_files[grepl("iid", index_files)]
+    } else { # compare only rw models for stitched indices
+      index_files <- index_files[grepl("rw", index_files)]
+    }
+
     if (length(index_files) == 0) {
       message("No indices found for: ", paste0(.sc, "/", sp_hyphens, "*"))
       return(invisible(NULL))
     }
 
-    purrr::map_dfr(index_files, \(i) {
-      readRDS(i)
-    }) |> take_min_aic()
+    purrr::map(index_files, readRDS) |>
+      purrr::keep(is.data.frame) |>
+      purrr::list_rbind() |>
+      take_min_aic()
   })
   saveRDS(ind_df, out_file)
 })
+
