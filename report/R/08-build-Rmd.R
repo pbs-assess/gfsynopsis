04b4090b018acd708611181c55f38ad2393fb1fb
Jillian Dunic <jdunic@gmail.com>
2025-07-03 13:36:30 -0700

    remove URLs

--- report/R/08-build-Rmd.R
+++ report/R/08-build-Rmd.R
@@ -1,6 +1,6 @@
 # Generate plot-pages.Rmd ---------------------------------------------
 # This is the guts of where the .tex / .Rmd figure page code gets made
-pull_gfbib(path = file.path(build_dir, "bib", "spp-refs.bib")) # sometimes delay after pushing gfbib changes to github
+pull_gfbib(path = file.path(build_dir, "bib", "spp-refs.bib"), rm_url = TRUE) # sometimes delay after pushing gfbib changes to github
 temp <- lapply(spp$species_common_name, \(x) generate_plotpages_Rmd(x = x, spp = spp))
 temp <- lapply(temp, function(x) paste(x, collapse = "\n"))
 temp <- paste(temp, collapse = "\n")
