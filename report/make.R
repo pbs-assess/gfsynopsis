28445d22289063bf0c8cdbebd3677af70ef74bbb
Jillian Dunic <jdunic@gmail.com>
2025-10-01 13:36:09 -0700

    render cleaned up version of spatial subset report

--- report/make.R
+++ report/make.R
@@ -21,16 +21,16 @@ if (length(args)) {
 # interactive <- FALSE
 
 library(here)
-source(here("report/R/01-settings.R"))
-# source(here("report/R/01-settings-haida.R"))
+# source(here("report/R/01-settings.R"))
+source(here("report/R/01-settings-haida.R"))
 source(here("report/R/02-packages.R"))
 source(here("report/R/03-load-data.R"))
 # source(here("report/R/04-survey-index-standardization.R"))
 # source(here("report/R/05-cpue-index-standardization.R"))
 source(here("report/R/06-build-figure-pages.R"))
-# source(here("report/R/07-cache-ggplots.R"))
-# source(here("report/R/08-build-Rmd.R"))
-# source(here("report/R/09-optimize-png.R"))
+source(here("report/R/07-cache-ggplots.R"))
+source(here("report/R/08-build-Rmd.R"))
+source(here("report/R/09-optimize-png.R"))
 
 if (FALSE) {
   setwd(here("report/tech-report-main/"))
