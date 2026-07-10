3117d1935ad9b1be53eee3ff9fa53d4717c12db1
Sean Anderson <sean@seananderson.ca>
2026-07-10 11:09:07 -0700

    Split and format survey set data

--- report/R/03-load-data.R
+++ report/R/03-load-data.R
@@ -18,3 +18,28 @@ spp <- left_join(spp, cos, by = "species_science_name")
 
 # UPDATE THE .CSV IN THIS EACH YEAR! see R/join_refs_spp.R
 spp <- join_refs_spp(spp, french = french)
+
+# split out the survey set data into individual species .rds
+# files to match historical formats and for loading speed
+# because the full df is large
+
+set_data <- readRDS(file.path(dc, "survey-sets.rds"))
+survey_set_dir <- file.path(dc, "survey-sets")
+dir.create(survey_set_dir, showWarnings = FALSE, recursive = TRUE)
+
+survey_set_spp <- split(set_data, set_data$species_common_name)
+survey_set_names <- setNames(spp$spp_w_hyphens, spp$species_common_name)
+missing_spp <- setdiff(names(survey_set_spp), names(survey_set_names))
+if (length(missing_spp)) {
+  stop(
+    "Species in survey set data missing from get_spp_names(): ",
+    paste(missing_spp, collapse = ", ")
+  )
+}
+
+for (sp in names(survey_set_spp)) {
+  saveRDS(
+    gfdata:::convert_to_old_sets(survey_set_spp[[sp]]),
+    file = file.path(survey_set_dir, paste0(survey_set_names[sp], ".rds"))
+  )
+}
