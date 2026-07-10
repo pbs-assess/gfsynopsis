db2b87d7f600be8bc094835b392d5817415da62a
Sean Anderson <sean@seananderson.ca>
2026-07-10 15:12:23 -0700

    Make run-stitching use more cores effectively

--- report/run-stitching.R
+++ report/run-stitching.R
@@ -6,10 +6,7 @@ sc_hbll_out_n <- file.path(stitch_cache, "hbll_outside_n")
 sc_hbll_out_s <- file.path(stitch_cache, "hbll_outside_s")
 sc_hbll_ins <- file.path(stitch_cache, "hbll_inside")
 sc_iphc <- file.path(stitch_cache, "iphc")
-sc_mssm <- file.path(stitch_cache, 'mssm')
-
-# survey_dat <- readRDS(file.path(dc, paste0(gfsynopsis:::clean_name(.sp), ".rds")))$survey_sets |>
-#     prep_stitch_dat(survey_dat = _, bait_counts = bait_counts)
+sc_mssm <- file.path(stitch_cache, "mssm")
 
 syns <- c("SYN HS", "SYN QCS", "SYN WCVI", "SYN WCHG")
 families <- c(
@@ -19,212 +16,210 @@ families <- c(
   "delta-gengamma-poisson-link"
 )
 
-# RW stitched regions (and MSSM WCVI)
-# -----------------------------------------
-furrr::future_walk(spp_vector, function(.sp) {
-# purrr::walk(spp_vector, function(.sp) {
-  # Load and clean survey data
-  survey_dat <- readRDS(file.path(dc, "survey-sets", paste0(gfsynopsis:::clean_name(.sp), ".rds"))) |>
+load_stitch_survey_data <- function(.sp) {
+  readRDS(file.path(dc, "survey-sets", paste0(gfsynopsis:::clean_name(.sp), ".rds"))) |>
     prep_stitch_dat(survey_dat = _, bait_counts = bait_counts)
+}
 
-  purrr::walk(families, function(.fam) {
-    # Stitched SYN surveys - all regions
-    get_stitched_index(
+load_iphc_stitch_data <- function(.sp) {
+  gfdata::load_iphc_dat(species = .sp) |>
+    prep_iphc_stitch_dat(survey_dat = _)
+}
+
+stitch_opts <- furrr::furrr_options(seed = NULL, scheduling = Inf)
+
+fit_rw_stitched_job <- function(.sp, .family, .job) {
+  survey_dat <- load_stitch_survey_data(.sp)
+
+  if (.job == "synoptic") {
+    return(get_stitched_index(
       survey_dat = survey_dat,
       species = .sp,
       survey_type = "synoptic",
       cutoff = 20,
       form = catch ~ 1,
-      family = .fam,
+      family = .family,
       spatial = "on",
       spatiotemporal = "rw",
       use_extra_time = TRUE,
-      offset = 'offset',
+      offset = "offset",
       cache = sc_synoptic,
       check_cache = TRUE,
       shapefile = shapefile
-    )
-     # MSSM WCVI
-    get_stitched_index(
-      survey_dat = survey_dat |> filter(survey_abbrev == "MSSM WCVI"),
+    ))
+  }
+
+  if (.job == "mssm") {
+    return(get_stitched_index(
+      survey_dat = survey_dat |> dplyr::filter(survey_abbrev == "MSSM WCVI"),
       species = .sp,
       survey_type = "MSSM WCVI",
       cutoff = 8,
       form = catch ~ 1,
-      family = .fam,
+      family = .family,
       spatial = "on",
       spatiotemporal = "rw",
       use_extra_time = FALSE,
       cache = sc_mssm,
       check_cache = TRUE,
       shapefile = shapefile
-    )
-  })
+    ))
+  }
 
-  # HBLL outside N/S
-  # -----------------
-  get_stitched_index(
-    survey_dat = filter(survey_dat, survey_abbrev %in% c("HBLL OUT N", "HBLL OUT S")),
-    survey_type = "hbll_outside",
-    species = .sp,
-    cutoff = 20,
-    form = catch ~ 1,
-    family = "nb2",
-    spatial = "on",
-    spatiotemporal = "rw",
-    use_extra_time = TRUE,
-    offset = 'offset',
-    cache = sc_hbll_out,
-    check_cache = TRUE,
-    shapefile = shapefile
-  )
+  stop("Unknown RW stitched job: ", .job)
+}
 
-  # HBLL OUT N st = rw
-  get_stitched_index(
-    survey_dat = filter(survey_dat, survey_abbrev == "HBLL OUT N"),
-    survey_type = "HBLL OUT N",
-    species = .sp,
-    cutoff = 10,
-    form = catch ~ 1,
-    family = "nb2",
-    spatial = "on",
-    spatiotemporal = "rw",
-    use_extra_time = TRUE,
-    offset = 'offset',
-    cache = sc_hbll_out_n,
-    check_cache = TRUE,
-    shapefile = shapefile
-  )
+fit_single_region_job <- function(.sp, .job) {
+  survey_dat <- load_stitch_survey_data(.sp)
 
-  # HBLL OUT S st = rw
-  get_stitched_index(
-    survey_dat = filter(survey_dat, survey_abbrev == "HBLL OUT S"),
-    survey_type = "HBLL OUT S",
-    species = .sp,
-    cutoff = 10,
-    form = catch ~ 1,
-    family = "nb2",
-    spatial = "on",
-    spatiotemporal = "rw",
-    use_extra_time = TRUE,
-    offset = 'offset',
-    cache = sc_hbll_out_s,
-    check_cache = TRUE,
-    shapefile = shapefile
-  )
+  if (.job == "hbll_outside_rw") {
+    return(get_stitched_index(
+      survey_dat = dplyr::filter(survey_dat, survey_abbrev %in% c("HBLL OUT N", "HBLL OUT S")),
+      survey_type = "hbll_outside",
+      species = .sp,
+      cutoff = 20,
+      form = catch ~ 1,
+      family = "nb2",
+      spatial = "on",
+      spatiotemporal = "rw",
+      use_extra_time = TRUE,
+      offset = "offset",
+      cache = sc_hbll_out,
+      check_cache = TRUE,
+      shapefile = shapefile
+    ))
+  }
 
-  # HBLL OUT N st = iid
-  get_stitched_index(
-    survey_dat = filter(survey_dat, survey_abbrev == "HBLL OUT N"),
-    survey_type = "HBLL OUT N",
-    species = .sp,
-    cutoff = 10,
-    form = catch ~ 0 + as.factor(year),
-    family = "nb2",
-    spatial = "on",
-    spatiotemporal = "iid",
-    use_extra_time = FALSE,
-    cache = sc_hbll_out_n,
-    check_cache = TRUE,
-    shapefile = shapefile
-  )
+  if (.job == "hbll_out_n_rw") {
+    return(get_stitched_index(
+      survey_dat = dplyr::filter(survey_dat, survey_abbrev == "HBLL OUT N"),
+      survey_type = "HBLL OUT N",
+      species = .sp,
+      cutoff = 10,
+      form = catch ~ 1,
+      family = "nb2",
+      spatial = "on",
+      spatiotemporal = "rw",
+      use_extra_time = TRUE,
+      offset = "offset",
+      cache = sc_hbll_out_n,
+      check_cache = TRUE,
+      shapefile = shapefile
+    ))
+  }
 
-  # HBLL OUT S st = iid
-  get_stitched_index(
-    survey_dat = filter(survey_dat, survey_abbrev == "HBLL OUT S"),
-    survey_type = "HBLL OUT S",
-    species = .sp,
-    cutoff = 10,
-    form = catch ~ 0 + as.factor(year),
-    family = "nb2",
-    spatial = "on",
-    spatiotemporal = "iid",
-    use_extra_time = FALSE,
-    cache = sc_hbll_out_s,
-    check_cache = TRUE,
-    shapefile = shapefile
-  )
+  if (.job == "hbll_out_s_rw") {
+    return(get_stitched_index(
+      survey_dat = dplyr::filter(survey_dat, survey_abbrev == "HBLL OUT S"),
+      survey_type = "HBLL OUT S",
+      species = .sp,
+      cutoff = 10,
+      form = catch ~ 1,
+      family = "nb2",
+      spatial = "on",
+      spatiotemporal = "rw",
+      use_extra_time = TRUE,
+      offset = "offset",
+      cache = sc_hbll_out_s,
+      check_cache = TRUE,
+      shapefile = shapefile
+    ))
+  }
 
-  # HBLL inside N/S
-  # -----------------
-  get_stitched_index(
-    survey_dat = filter(survey_dat, survey_abbrev %in% c("HBLL INS N", "HBLL INS S")),
-    survey_type = "hbll_inside",
-    species = .sp,
-    cutoff = 20,
-    form = catch ~ 1,
-    family = "nb2",
-    spatial = "on",
-    spatiotemporal = "rw",
-    use_extra_time = TRUE,
-    offset = 'offset',
-    cache = sc_hbll_ins,
-    check_cache = TRUE,
-    shapefile = shapefile
-  )
+  if (.job == "hbll_out_n_iid") {
+    return(get_stitched_index(
+      survey_dat = dplyr::filter(survey_dat, survey_abbrev == "HBLL OUT N"),
+      survey_type = "HBLL OUT N",
+      species = .sp,
+      cutoff = 10,
+      form = catch ~ 0 + as.factor(year),
+      family = "nb2",
+      spatial = "on",
+      spatiotemporal = "iid",
+      use_extra_time = FALSE,
+      cache = sc_hbll_out_n,
+      check_cache = TRUE,
+      shapefile = shapefile
+    ))
+  }
 
- # IPHC FISS
- # -----------------------------------------
-   iphc_dat <- gfdata::load_iphc_dat(species = .sp) |>
-     prep_iphc_stitch_dat(survey_dat = _)
-   get_stitched_index(
-     survey_dat = iphc_dat,
-     species = .sp,
-     survey_type = "IPHC FISS",
-     form = catch ~ 1,
-     family = "nb2",
-     time = 'year',
-     spatial = 'on',
-     spatiotemporal = 'rw',
-     use_extra_time = TRUE,
-     offset = 'offset',
-     gradient_thresh = 0.001,
-     cutoff = 20,
-     cache = sc_iphc,
-     check_cache = TRUE,
-     shapefile = shapefile
-   )
-})
+  if (.job == "hbll_out_s_iid") {
+    return(get_stitched_index(
+      survey_dat = dplyr::filter(survey_dat, survey_abbrev == "HBLL OUT S"),
+      survey_type = "HBLL OUT S",
+      species = .sp,
+      cutoff = 10,
+      form = catch ~ 0 + as.factor(year),
+      family = "nb2",
+      spatial = "on",
+      spatiotemporal = "iid",
+      use_extra_time = FALSE,
+      cache = sc_hbll_out_s,
+      check_cache = TRUE,
+      shapefile = shapefile
+    ))
+  }
 
-# Fit individual SYN surveys
-# -----------------------------------------
-tofit <- tidyr::expand_grid(.sp = spp_vector, .syn = syns, .family = families)
-furrr::future_pmap(tofit, function(.sp, .syn, .family) {
-# purrr::pmap(tofit, function(.sp, .syn, .family) {
+  if (.job == "hbll_inside_rw") {
+    return(get_stitched_index(
+      survey_dat = dplyr::filter(survey_dat, survey_abbrev %in% c("HBLL INS N", "HBLL INS S")),
+      survey_type = "hbll_inside",
+      species = .sp,
+      cutoff = 20,
+      form = catch ~ 1,
+      family = "nb2",
+      spatial = "on",
+      spatiotemporal = "rw",
+      use_extra_time = TRUE,
+      offset = "offset",
+      cache = sc_hbll_ins,
+      check_cache = TRUE,
+      shapefile = shapefile
+    ))
+  }
 
+  if (.job == "iphc_rw") {
+    return(get_stitched_index(
+      survey_dat = load_iphc_stitch_data(.sp),
+      species = .sp,
+      survey_type = "IPHC FISS",
+      form = catch ~ 1,
+      family = "nb2",
+      time = "year",
+      spatial = "on",
+      spatiotemporal = "rw",
+      use_extra_time = TRUE,
+      offset = "offset",
+      gradient_thresh = 0.001,
+      cutoff = 20,
+      cache = sc_iphc,
+      check_cache = TRUE,
+      shapefile = shapefile
+    ))
+  }
+
+  stop("Unknown single-region job: ", .job)
+}
+
+fit_synoptic_single_region_job <- function(.sp, .syn, .family) {
   .cache <- file.path(stitch_cache, paste0("synoptic-", .syn))
-  spp_filename <- paste0(gfsynopsis:::clean_name(.sp), "_", .family, "_sp-on-st-rw", ".rds")
+  spp_filename <- paste0(gfsynopsis:::clean_name(.sp), "_", .family, "_sp-on-st-iid", ".rds")
   stitch_cached_sp <- file.path(.cache, spp_filename)
 
-  if(!file.exists(stitch_cached_sp)) {
-    survey_dat <- readRDS(file.path(dc, paste0(gfsynopsis:::clean_name(.sp), ".rds")))$survey_sets |>
-      prep_stitch_dat(survey_dat = _, bait_counts = bait_counts) |>
-      filter(survey_abbrev == .syn)
-    if (.syn == "SYN WCHG") {
-      survey_dat <- filter(survey_dat, year != 2014) # partial year
-      .cutoff <- 8
-    } else {
-      .cutoff <- 10
-    }
+  if (file.exists(stitch_cached_sp)) {
+    return(invisible(NULL))
   }
-  # RW individual SYN
-#  get_stitched_index(
-#    survey_dat = survey_dat,
-#    species = .sp,
-#    survey_type = .syn,
-#    cutoff = .cutoff,
-#    form = catch ~ 1,
-#    family = .family,
-#    spatial = "on",
-#    spatiotemporal = "rw",
-#    use_extra_time = TRUE,
-#    offset = 'offset',
-#    cache = .cache,
-#    check_cache = TRUE,
-#    shapefile = shapefile
-#  )
 
-  # IID individual SYN
+  survey_dat <- load_stitch_survey_data(.sp) |>
+    dplyr::filter(survey_abbrev == .syn)
+
+  if (.syn == "SYN WCHG") {
+    survey_dat <- dplyr::filter(survey_dat, year != 2014)
+    .cutoff <- 8
+  } else {
+    .cutoff <- 10
+  }
+
   get_stitched_index(
     survey_dat = survey_dat,
     species = .sp,
@@ -235,12 +230,54 @@ furrr::future_pmap(tofit, function(.sp, .syn, .family) {
     spatial = "on",
     spatiotemporal = "iid",
     use_extra_time = FALSE,
-    offset = 'offset',
+    offset = "offset",
     cache = .cache,
     check_cache = TRUE,
     shapefile = shapefile
   )
-})
+}
+
+# RW stitched regions (and MSSM WCVI)
+# -----------------------------------------
+rw_jobs <- tidyr::expand_grid(
+  .sp = spp_vector,
+  .family = families,
+  .job = c("synoptic", "mssm")
+)
+furrr::future_pwalk(
+  rw_jobs,
+  fit_rw_stitched_job,
+  .options = stitch_opts
+)
+
+# HBLL / IPHC stitched jobs
+# -----------------------------------------
+single_region_jobs <- tidyr::expand_grid(
+  .sp = spp_vector,
+  .job = c(
+    "hbll_outside_rw",
+    "hbll_out_n_rw",
+    "hbll_out_s_rw",
+    "hbll_out_n_iid",
+    "hbll_out_s_iid",
+    "hbll_inside_rw",
+    "iphc_rw"
+  )
+)
+furrr::future_pwalk(
+  single_region_jobs,
+  fit_single_region_job,
+  .options = stitch_opts
+)
+
+# Fit individual SYN surveys
+# -----------------------------------------
+tofit <- tidyr::expand_grid(.sp = spp_vector, .syn = syns, .family = families)
+furrr::future_pwalk(
+  tofit,
+  fit_synoptic_single_region_job,
+  .options = stitch_opts
+)
 
 # ------------------------------------------------------------------------------
 # Get best model
@@ -248,7 +285,7 @@ furrr::future_pmap(tofit, function(.sp, .syn, .family) {
 
 take_min_aic <- function(x) {
   if (nrow(x)) {
-    filter(x, aic == min(aic))
+    dplyr::filter(x, aic == min(aic))
   }
 }
 
@@ -267,9 +304,9 @@ purrr::map_dfr(spp_vector, function(.sp) {
     cache_files <- list.files(file.path(stitch_cache, .sc), full.names = TRUE)
     index_files <- cache_files[grepl(sp_hyphens, cache_files)]
 
-    if (.sc %in% single_regions) { # compare only iid models for single regions
+    if (.sc %in% single_regions) {
       index_files <- index_files[grepl("iid", index_files)]
-    } else { # compare only rw models for stitched indices
+    } else {
       index_files <- index_files[grepl("rw", index_files)]
     }
 
@@ -285,4 +322,3 @@ purrr::map_dfr(spp_vector, function(.sp) {
   })
   saveRDS(ind_df, out_file)
 })
-
