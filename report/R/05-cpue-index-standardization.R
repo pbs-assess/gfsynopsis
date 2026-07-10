d76c9624d41756850ab55c22d61dfb4cdb27af94
Sean Anderson <sean@seananderson.ca>
2026-07-10 16:02:55 -0700

    8 cores

--- report/R/05-cpue-index-standardization.R
+++ report/R/05-cpue-index-standardization.R
@@ -1,7 +1,7 @@
 # CPUE model fits -----------------------------------------------------
 
 d_cpue <- readRDS(file.path(dc, "cpue-index-dat.rds"))
-if (parallel_processing && is_hake_server()) future::plan(future::multicore, workers = 4L)
+if (parallel_processing && is_hake_server()) future::plan(future::multicore, workers = 8L)
 if (!is_hake_server()) future::plan(future::sequential)
 source(here("report/cpue-sdmTMB.R"))
 future::plan(sequential)
