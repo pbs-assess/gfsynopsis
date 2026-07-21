7ab059b691708c65ce2bcddf9adb134f768f535e
Sean Anderson <sean@seananderson.ca>
2026-07-21 16:41:49 -0700

    Formatting

--- report/R/08-build-Rmd.R
+++ report/R/08-build-Rmd.R
@@ -1,10 +1,12 @@
 # Generate plot-pages.Rmd ---------------------------------------------
 # This is the guts of where the .tex / .Rmd figure page code gets made
 pull_gfbib(path = file.path(build_dir, "bib", "spp-refs.bib"), rm_url = TRUE) # sometimes delay after pushing gfbib changes to github
-temp <- lapply(spp$species_common_name, \(x) generate_plotpages_Rmd(x = x, spp = spp))
+temp <- lapply(
+  spp$species_common_name,
+  \(x) generate_plotpages_Rmd(x = x, spp = spp, french = french, ext = ext)
+)
 temp <- lapply(temp, function(x) paste(x, collapse = "\n"))
 temp <- paste(temp, collapse = "\n")
 temp <- c("<!-- This page has been automatically generated: do not edit by hand -->\n", temp)
 con <- file(file.path(build_dir, "plot-pages.Rmd"), encoding = "UTF-8")
 writeLines(temp, con = con)
-
